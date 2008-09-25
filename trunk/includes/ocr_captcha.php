a8e14bbd523f2ab601189112a4f99a7a05d1a27b
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2008-09-25 05:17:41 +0000

    - fix ban system - fix captcha [ rendreing error ] - remove SQL percentage in the footer stats

--- trunk/includes/ocr_captcha.php
+++ trunk/includes/ocr_captcha.php
@@ -26,7 +26,8 @@ class ocr_captcha
     var $filename;  // file of captcha picture stored on disk
     var $imagetype="png"; // can also be "png";
     var $public_key;    // public key
-    var $font_file="./includes/DUNGEON.TTF";
+    var $font_file="arial.ttf";
+   //var $font_file="./includes/DUNGEON.TTF"; for more complix
     var $font_file2="./includes/DUNGEON.GDF";
 	
 	
@@ -63,11 +64,13 @@ class ocr_captcha
 	//(not to be public!!), all you have to do is here to change the algorithm
     function generate_private($public="")
 	{
-      if ($public=="")
-	  {
-        $public=$this->public_key;
-      }
-	  return substr(md5($this->key.$public),16-$this->long/2,$this->long);
+		if ($public=="")
+		{
+			$public=$this->public_key;
+		}
+		
+		$d =  substr(md5($this->key.$public),16-$this->long/2,$this->long);
+    	return str_replace(array('o','O','0'), '', $d); //remove zero and o
     }
     
     // check if the public text is link to the private text
@@ -95,12 +98,9 @@ class ocr_captcha
     // display a captcha picture with private text and return the public text
     function make_captcha($noise=true)
 	{
-	   $private_key	= $this->generate_private();
-		  
-
+		$private_key	= $this->generate_private();
 		$image		= imagecreatetruecolor($this->lx,$this->ly);
 
-	
       $back=ImageColorAllocate($image,intval(rand(224,255)),intval(rand(224,255)),intval(rand(224,255)));
       ImageFilledRectangle($image,0,0,$this->lx,$this->ly,$back);
       if ($noise)
@@ -108,7 +108,7 @@ class ocr_captcha
 	        for ($i=0;$i<$this->nb_noise;$i++)
 			{
 				$size=intval(rand(6,14));
-				$angle=intval(rand(0,360));
+				$angle=intval(rand(3,360));
 				$x=intval(rand(10,$this->lx-10));
 				$y=intval(rand(0,$this->ly-5));
 				$color=imagecolorallocate($image,intval(rand(160,224)),intval(rand(160,224)),intval(rand(160,224)));
@@ -116,8 +116,6 @@ class ocr_captcha
 		         
 				//fix in 1rc4  
 				$this->klg_ImageTTFText($image, $size, $angle, $x, $y, $color, $text);
-
-
 	        }
       }
       else
@@ -137,11 +135,12 @@ class ocr_captcha
       // private text to read
       for ($i=0,$x=5; $i<$this->long;$i++)
 	  {
-	        $r=intval(rand(0,128));
-	        $g=intval(rand(0,128));
-	        $b=intval(rand(0,128));
+	        $r=intval(rand(10,128));
+	        $g=intval(rand(10,128));
+	        $b=intval(rand(10,128));
 	        $color = ImageColorAllocate($image, $r,$g,$b);
 	        $shadow= ImageColorAllocate($image, $r+128, $g+128, $b+128);
+
 	        $size=intval(rand(12,17));
 	        $angle=intval(rand(-30,30));
 	        $text=strtoupper(substr($private_key,$i,1));

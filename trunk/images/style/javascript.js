67875495228ee23b86104545b79035d04f9325e9
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2008-10-23 22:30:57 +0000

    - fix install config step 406 error - fix bug if link not ended with slash / - fix bug with htaccess - fix bug with gzip - add some hooks in upload form - remove "Powered by Kleeja" in header

--- trunk/images/style/javascript.js
+++ trunk/images/style/javascript.js
@@ -5,7 +5,9 @@
 
 	var totalupload_num=number_of_uploads-1;
 	
-	function $(id) {
+	//get element by id name
+	function $(id)
+	{
 		if(document.all){
          	return eval("document.all['" + id + "']");
     	}
@@ -14,37 +16,51 @@
 		}
 	}
 	
-	function makeupload(type){
+	//filds of upload 
+	function makeupload(type)
+	{
 		var value = (type==1) ?  $("upload_num").value : $("upload_f_num").value;
 		var upload_forum3 = (type==1) ? $("upload_forum") :  $("upload_f_forum");
-		var upload_show_num="";
-		uploaded=2;
+		var upload_show_num = "";
+		uploaded=1;
 		upload_num=(value-1);
 		if(upload_num>totalupload_num){	upload_num=totalupload_num;	}
-		for(i=0;i<upload_num;i++){
+		
+		for(i=0;i<upload_num;i++)
+		{
 			thisuid = uploaded+i;
-			upload_show_num =(type==1) ? upload_show_num + '<input type="file" name="file[]"><br>' : upload_show_num + '<input type="text" size="50" name="file['+thisuid+']" value="' + LANG_PAST_URL_HERE + '" style="color:silver;" dir="ltr"><br>';
+			upload_show_num += (type==1) ?  '<input type="file" id="file[]" name="file[]" /><br />' : '<input type="text" size="50" id="file[]" name="file['+thisuid+']" value="' + LANG_PAST_URL_HERE + '" style="color:silver;" dir="ltr" /><br />';
 		}
 		upload_forum3.innerHTML  = upload_show_num;
 	
-		
-		
 	}
-	function plus (type){
+	
+	//new field
+	function plus (type)
+	{
 		var value = (type==1) ?  $("upload_num") : $("upload_f_num");
 		var num = number_of_uploads;
-			if (value.value < num ){
+		if (value.value < num )
+		{
 			value.value++;
-			}else{
+		}
+		else
+		{
 			alert(LANG_MORE_F_FILES);
-			}
+		}
 	}
-	function minus (type){
+	
+	//remove field
+	function minus (type)
+	{
 		var value = (type==1) ?  $("upload_num") : $("upload_f_num");
 		var num = number_of_uploads;
 		if (value.value != 1 )value.value--;
 	}
-	function form_submit() {
+	
+	//submit
+	function form_submit() 
+	{
 		var load = $('loadbox');
 		var uploader = $("uploader");
 		load.style.display = "inline";
@@ -52,20 +68,27 @@
 		document.uploader.submit();
 	}
 	
-	function accept_terms (sub,ch){
+	//acceept terms of uploads
+	function accept_terms (sub,ch)
+	{
 		var submit = $(sub);
 		var checker = $(ch);
-		if ( checker.checked ){submit.disabled = ""; }else{submit.disabled = "disabled"; }
+		submit.disabled = ( checker.checked ) ?  "" : "disabled";
 	}
-
-	function showhide() {
+	
+	//show or hide some fields
+	function showhide()
+	{
 		var txt = $("texttype");
 		var fle = $("filetype");
 
-		if (txt.style.display == 'none'){
+		if (txt.style.display == 'none')
+		{
 			txt.style.display = 'block';
 			fle.style.display = 'none';
-		}else{
+		}
+		else
+		{
 			fle.style.display = 'block';
 			txt.style.display = 'none';
 		}

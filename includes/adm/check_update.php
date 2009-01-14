bc565bb809a2686d6450ce3c839690462be816d2
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2009-01-14 03:59:30 +0000

    admin syatem , updating check system

--- includes/adm/check_update.php
+++ includes/adm/check_update.php
@@ -34,14 +34,58 @@
 		}
 		else if (version_compare(strtolower(KLEEJA_VERSION), strtolower($version_data), '='))
 		{
+			
+			//check if there is any pre-release
+			$pre_version_data =	fetch_remote_file('http://www.kleeja.com/check_vers/ver_pre.txt');
+			
+			if(trim($pre_version_data) != '')
+			{
+				$version_data = $pre_version_data;
+			}
+			
+			
 			$text	= $lang['U_LAST_VER_KLJ'];
 			$stylee	= "admin_info";
+			
 		}
 		else if (version_compare(strtolower(KLEEJA_VERSION), strtolower($version_data), '>'))
 		{
 			$text	= 'You are using pre-release version , click <a href="http://www.kleeja.com/bugs/">here</a> to tell us about any bug you face it.';
 			$stylee	= "admin_info";
 		}
+		
+		//lets recore it
+		$v = unserialize($config['new_version']);
+	
+		if(version_compare(strtolower($v['version_number']), strtolower($version_data), '<') || isset($_GET['show_msg']))
+		{
+			$data	= array('version_number'	=> $version_data,
+							'last_check'		=> time(),
+							'msg_appeared'		=> isset($_GET['show_msg']) ? true : false, 
+							'pre_release'		=> (trim($pre_version_data) != '')? true : false
+						);
+			
+
+			$data = serialize($data);
+			
+			$update_query = array(
+									'UPDATE'	=> "{$dbprefix}config",
+									'SET'		=> "value='"  . addslashes($data) . "'",
+									'WHERE'		=> "name='new_version'"
+									);
+
+			if (!$SQL->build($update_query)) die($lang['CANT_UPDATE_SQL']);
+			
+			//clean cache
+			delete_cache('data_config');
+			
+			
+			//then go back  to start
+			if(isset($_GET['show_msg']))
+			{
+				header('location: ./admin.php');
+			}
+		}	
 	}
 
 

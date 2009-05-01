f68e7c16099ebbe06bf4b7f50c215a1569ba645e
phpfalcon <phpfalcon@940b39b9-983a-0410-8f00-47514fe486d8>
2009-05-01 16:31:08 +0000

    fixed upload from link

--- trunk/includes/KljUploader.php
+++ trunk/includes/KljUploader.php
@@ -474,6 +474,10 @@ function process ()
 											else
 											{
 												$data = fetch_remote_file($_POST['file'][$i], $this->folder . "/" . $this->filename2);
+												//then ..write new file
+												$fp2 = fopen($this->folder . "/" . $this->filename2, "w");
+												fwrite($fp2, $data);
+												fclose($fp2);
 											}
 										}
 										else

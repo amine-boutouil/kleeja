b5d80470f138bc8515d0702e6cdd0a9bd95c1bc7
phpfalcon <phpfalcon@940b39b9-983a-0410-8f00-47514fe486d8>
2009-05-04 18:59:45 +0000

    fixed tiny bug

--- trunk/index.php
+++ trunk/index.php
@@ -159,7 +159,7 @@
 		}
 		
 		$most_online = $stat_most_user_online_ever; 
-		$on_muoe	 = gmdate("d-m-Y H:a", $stat_last_muoe);
+		$on_muoe	 = (!empty($stat_last_muoe)) ? gmdate("d-m-Y H:a", $stat_last_muoe) : '';
 		($hook = kleeja_run_hook('if_online_index_page')) ? eval($hook) : null; //run hook	
 	
 	}#allow_online

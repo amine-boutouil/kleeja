c6bbf16ade68bee0d88f1a4ab9fbc8b7ebdac239
altar3q <altar3q@940b39b9-983a-0410-8f00-47514fe486d8>
2009-05-14 22:26:16 +0000

    Final design of the control panel

--- lang/ar/common.php
+++ lang/ar/common.php
@@ -230,7 +230,7 @@ $lang = array_merge($lang, array(
 	'SIZE_U' => 'الحجم [م]',
 	'ALLOW_G' => 'سماح [ز]',
 	'ALLOW_U' => 'سماح [م]',
-	'E_EXTS' => '<b>ز</b>:تعني الزوار <br /> <b>م</b>: تعني الأعضاء  <br />الأحجام تظبط بالكليوبايت.',
+	'E_EXTS' => '<b><font color="#008000">[&nbsp;ز&nbsp;]&nbsp;:&nbsp;  تعني الزوار</font></b>&nbsp;/&nbsp;<b><font color="#D80000">[&nbsp;م&nbsp;]&nbsp;:&nbsp; تعني الأعضاء </font></b>&nbsp;/&nbsp;<i>الأحجام تظبط بالكليوبايت</i>',
 	'UPDATED_EXTS' => 'تم تحديث الإمتدادات بنجاح',
 	'UPDATE_FILES' => 'تحديث الملفات',
 	'BY' => 'من',

cb70587157d5646109884617ed9eb13df337d127
phpfalcon <phpfalcon@940b39b9-983a-0410-8f00-47514fe486d8>
2009-06-15 16:41:45 +0000

    improve info and error admin functions

--- trunk/includes/functions.php
+++ trunk/includes/functions.php
@@ -872,48 +872,7 @@ function kleeja_admin_err($msg, $navigation=true, $title='', $exit=true)
 	// assign {text} in err template
 	$text	= $msg;
 	$SHOW_LIST	= $navigation;
-	$adm_extensions_menu = $adm_extensions_menu;
-	/*
-	//get adm extensions
-	if($SHOW_LIST)
-	{
-		//exception extentions
-		$ext_expt	= array();
-		$ext_expt[]	= 'start';
-		//confirm msgs
-		$ext_confirm	= array();
-		$ext_confirm[]	= 'repair';	
-		$ext_confirm[]	= 'lgoutcp';	
-		
-		//no extensions ?
-		if(!$adm_extensions || !is_array($adm_extensions))
-		{
-			big_error('No Extensions', 'ERROR IN LOADING ADMIN EXTENSIONS !');
-		}
-	
-		//make array for menu 
-		$adm_extensions_menu	=	array();
-	
-		//re oreder the items as alphapatic !
-		sort($adm_extensions);
-		$i = 0;
 
-		foreach($adm_extensions as $m)
-		{
-			//some exceptions
-			if(@in_array($m, $ext_expt))
-			{
-				continue;
-			}
-		
-			++$i;
-			$adm_extensions_menu[$i]	= array('icon'	=> (file_exists($STYLE_PATH_ADMIN . 'images/' . ($m == 'configs' ? 'options' : $m) . '_button.gif'))	? $STYLE_PATH_ADMIN . 'images/' . ($m == 'configs' ? 'options' : $m) . '_button.gif' : $STYLE_PATH_ADMIN . 'images/no_icon_button.gif',
-											'lang'	=> !empty($lang['R_'. strtoupper($m)]) ? $lang['R_'. strtoupper($m)]: (!empty($lang[strtoupper($m)]) ? $lang[strtoupper($m)] :  (!empty($olang[strtoupper($m)]) ? $olang[strtoupper($m)] : strtoupper($m))),
-											'link'	=> 'admin.php?cp=' . ($m == 'configs' ? 'options' : $m),
-											'confirm'	=> (@in_array($m, $ext_confirm)) ? true : false,
-											);
-		}
-	}*/
 	//header
 	echo $tpl->display("admin_header");
 	//show tpl
@@ -943,51 +902,6 @@ function kleeja_admin_info($msg, $navigation=true, $title='', $exit=true)
 // assign {text} in err template
 	$text	= $msg;
 	$SHOW_LIST	= $navigation;
-	
-	
-	//get adm extensions
-	if($SHOW_LIST)
-	{
-		//path of admin extensions
-		$path_adm	= "includes/adm";
-
-		//exception extentions
-		$ext_expt	= array();
-		$ext_expt[]	= 'start';
-		//confirm msgs
-		$ext_confirm	= array();
-		$ext_confirm[]	= 'repair';	
-		$ext_confirm[]	= 'lgoutcp';	
-
-		//no extensions ?
-		if(!$adm_extensions || !is_array($adm_extensions))
-		{
-			big_error('No Extensions', 'ERROR IN LOADING ADMIN EXTENSIONS !');
-		}
-	
-		//make array for menu 
-		$adm_extensions_menu	=	array();
-	
-		//re oreder the items as alphapatic !
-		sort($adm_extensions);
-		$i = 0;
-	
-		foreach($adm_extensions as $m)
-		{
-			//some exceptions
-			if(@in_array($m, $ext_expt))
-			{
-				continue;
-			}
-		
-			++$i;
-			$adm_extensions_menu[$i]	= array('icon'	=> (file_exists($STYLE_PATH_ADMIN . 'images/' . ($m == 'configs' ? 'options' : $m) . '_button.gif'))	? $STYLE_PATH_ADMIN . 'images/' . ($m == 'configs' ? 'options' : $m) . '_button.gif' : $STYLE_PATH_ADMIN . 'images/no_icon_button.gif',
-											'lang'	=> !empty($lang['R_'. strtoupper($m)]) ? $lang['R_'. strtoupper($m)]: (!empty($lang[strtoupper($m)]) ? $lang[strtoupper($m)] :  (!empty($olang[strtoupper($m)]) ? $olang[strtoupper($m)] : strtoupper($m))),
-											'link'	=> 'admin.php?cp=' . ($m == 'configs' ? 'options' : $m),
-											'confirm'	=> (@in_array($m, $ext_confirm)) ? true : false,
-											);
-		}
-	}
 	
 	//header
 	echo $tpl->display("admin_header");

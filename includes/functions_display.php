84e416bb12ecf4d48698ef15aa393c46c4d7e313
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2009-07-15 20:34:19 +0000

    fix many notices and extra header and footer #2

--- includes/functions_display.php
+++ includes/functions_display.php
@@ -16,7 +16,7 @@
 */	
 function Saaheader($title, $outscript=false)
 {
-		global $tpl, $usrcp, $lang, $user_is, $config;
+		global $tpl, $usrcp, $lang, $olang, $user_is, $config;
 		global $extras, $script_encoding, $errorpage;
 
 		$user_is = ($usrcp->name()) ? true: false;
@@ -126,7 +126,7 @@ function Saaheader($title, $outscript=false)
 function Saafooter($outscript=false)
 {
 		global $tpl, $SQL, $starttm, $config, $usrcp, $lang, $olang;
-		global $do_gzip_compress, $script_encoding, $errorpage, $footer;
+		global $do_gzip_compress, $script_encoding, $errorpage, $extras;
 		
 		//show stats ..
 		$page_stats = '';

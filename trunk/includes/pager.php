337ac04210f8a5bbef41f7a91dc26624415d3749
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2009-08-08 18:38:00 +0000

    add new feature in acp #12

--- trunk/includes/pager.php
+++ trunk/includes/pager.php
@@ -2,11 +2,10 @@
 ##################################################
 #						Kleeja 
 #
-# Filename : common.php 
-# purpose :  pages system 
-# copyright 2007-2008 Kleeja.com ..
-#license http://opensource.org/licenses/gpl-license.php GNU Public License
-# Author : Based on class from phpclasses.org, i forgot who was.
+# Filename : pager.php 
+# purpose :  pagination system 
+# copyright 2007-2009 Kleeja.com ..
+# license http://opensource.org/licenses/gpl-license.php GNU Public License
 # $Author$ , $Rev$,  $Date::                           $
 ##################################################
 
@@ -17,52 +16,44 @@ if (!defined('IN_COMMON'))
 }
 
 class SimplePager
-{ 
+{
+    var $totalPages, $startRow , $currentPage;
 
-    var $totalPages; 
-    var $startRow; 
-	var $currentPage;
-	
-	
-    function SimplePager($rowsPerPage,$numRows,$currentPage=1)
+	/**
+	*Construct function ..
+	*
+	*/
+    function SimplePager($rowsPerPage, $numRows, $currentPage = 1)
 	{ 
         // Calculate the total number of pages 
-        $this->totalPages	=	ceil($numRows/$rowsPerPage); 
-		
+        $this->totalPages = ceil($numRows/$rowsPerPage); 
+
         // Check that a valid page has been provided 
-        if ($currentPage < 1) 
-		{
-            $currentPage	=	1; 
-		}
-        else if ($currentPage > $this->totalPages) 
-        {
-			$currentPage=$this->totalPages;
-		}
-		
+		$this->currentPage = $currentPage < 1 ? 1 :  ($currentPage > $this->totalPages ? $this->totalPages : $currentPage); 
+
         // Calculate the row to start the select with 
-        $this->startRow=(($currentPage - 1) * $rowsPerPage); 
-		$this->currentPage = $currentPage;
-    } 
-	
+        $this->startRow = ($this->currentPage - 1) * $rowsPerPage; 
+    }
+
     function getTotalPages ()
 	{
 		return $this->totalPages;
 	} 
-	
-    function getStartRow()
+
+	function getStartRow()
 	{
 		return $this->startRow;
-	} 
-	
+	}
+
 	function print_nums($link)
 	{
 		global $lang, $config;
-		
+
 		//if no page
 		if($this->totalPages <= 1) return;
-		
-		$re 	= '<div class="pagination">';
-		
+
+		$re = '<div class="pagination">';
+
 		// Add a previous page link
 		if ($this->totalPages > 1 && $this->currentPage > 1)
 			($config['mod_writer'] && !defined('IN_ADMIN')) ? $re .= '<a class="paging" href="' . $link . '-' . ($this->currentPage-1) . '.html">'. $lang['PREV'] .'</a>&nbsp;&nbsp;' : $re .= '<a class="paging" href="' . $link . '&amp;page=' . ($this->currentPage-1) . '">'. $lang['PREV'] .'</a>&nbsp;&nbsp;';
@@ -80,28 +71,22 @@ class SimplePager
 			else
 				$re .= '<strong class="here_page">'. $current .'</strong>&nbsp;';
 		}
-		
+
 		if ($this->currentPage <= ($this->totalPages-3))
 		{
 			if ($this->currentPage != ($this->totalPages-3) && $this->currentPage != ($this->totalPages-4))
 				$re .= '...';
-
 			($config['mod_writer'] && !defined('IN_ADMIN')) ? $re .= '<a href="' . $link . '-' . ($this->totalPages) . '.html"  class="paging">'. $this->totalPages .'</a>' : $re .= '<a href="' . $link . '&amp;page=' . ($this->totalPages) . '"  class="paging">'. $this->totalPages .'</a>';
 		}
-		
+
 		// Add a next page link
 		if ($this->totalPages > 1 && $this->currentPage < $this->totalPages)
 			($config['mod_writer'] && !defined('IN_ADMIN')) ? $re .= '&nbsp;&nbsp;<a class="paging" href="' . $link . '-' . ($this->currentPage+1) . '.html">'. $lang['NEXT'] .'</a>' :  $re .= '&nbsp;&nbsp;<a class="paging" href="' . $link . '&amp;page=' . ($this->currentPage+1) . '">'. $lang['NEXT'] .'</a>';
-			
+
 		$re .= '</div>'; 
-		
-		/*
-			if($this->totalPages < 2) return;
-			$re = ($this->currentPage>1) ? '<a href="'.$link.'&amp;page='.($this->currentPage-1).'" class=paging>'. $lang['PREV'] .'</a> ': '';
-			for($s=1;$s<$this->totalPages+1;$s++){$re .= ($this->currentPage==$s)?"<span class=here_psge>$s</span>":"<a href=".$link."&amp;page=$s class=paging>$s</a> ";}
-			$re .= ($this->currentPage<$this->totalPages) ? ' <a href="'.$link.'&amp;page='.($this->currentPage+1).'" class=paging>'. $lang['NEXT'] .'</a>': '';
-		*/
+
 		return $re;
 	}
-} 
-?>
+}
+
+#<-- EOF

d12dd7f8a56df4ccb588a92c3a6b5abb09b74c3e
phpfalcon <phpfalcon@940b39b9-983a-0410-8f00-47514fe486d8>
2009-08-10 05:57:43 +0000

    fix integration system

--- trunk/includes/auth_integration/vb.php
+++ trunk/includes/auth_integration/vb.php
@@ -59,8 +59,11 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 	}
 				
 	$SQLVB	= new SSQL($forum_srv, $forum_user, $forum_pass, $forum_db, true);
-	$charset_db = @mysql_client_encoding($SQLVB->connect_id);
-	mysql_query("SET NAMES '" . $charset_db . "'");
+	if(!preg_match('/utf/i',strtolower($script_encoding)))
+	{
+		$charset_db = @mysql_client_encoding($SQLVB->connect_id);
+		mysql_query("SET NAMES '" . $charset_db . "'");
+	}
 	//$mysql_version = @mysql_get_server_info($SQLVB->connect_id);
 				
 	unset($forum_pass); // We do not need this any longe

c12451b9532d55e068b5b099c7561e3e1f555ac3
phpfalcon <phpfalcon@940b39b9-983a-0410-8f00-47514fe486d8>
2009-08-10 23:27:34 +0000

    fix acp login #2

--- admin/index.php
+++ admin/index.php
@@ -19,6 +19,11 @@
 	{
 		define('IN_ADMIN_LOGIN', true);
 	}
+	
+	if(isset($_GET['go']) && $_GET['go'] == 'login' && isset($_POST['submit']))
+	{
+		define('IN_ADMIN_LOGIN_POST', true);
+	}
 
 	
 	//include imprtant file ..
@@ -33,13 +38,12 @@
 	
 
 	//for security
-	/*
-	if (!$usrcp->name())
+	if (!$usrcp->name() && !defined('IN_ADMIN_LOGIN_POST'))
 	{
 		($hook = kleeja_run_hook('user_not_admin_admin_page')) ? eval($hook) : null; //run hook 
 		redirect(PATH . 'ucp.php?go=login&return=' . str_replace(array('?', '/', '='), array('ooklj1oo', 'ooklj2oo', 'ooklj3oo'), ADMIN_PATH . '?cp=' . $go_to));
 	}
-	*/
+	
 	
 	//need to login again
 	if((empty($_SESSION['ADMINLOGIN']) || $_SESSION['ADMINLOGIN'] != md5($usrcp->name() . $config['siteurl'])) || (empty($_SESSION['USER_SESS']) || $_SESSION['USER_SESS'] != session_id()))

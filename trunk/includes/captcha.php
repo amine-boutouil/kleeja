b7f9f3d16c9e23d0f8a1a1dc5fd6040406b72f15
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2009-08-15 12:35:04 +0000

    fix captcha

--- trunk/includes/captcha.php
+++ trunk/includes/captcha.php
@@ -31,38 +31,46 @@ exit();
 //
 function kleeja_cpatcha_image()
 {
-    //Let's generate a totally random string using md5
-    $md5_hash = md5(rand(0,999)); 
-    //We don't need a 32 character long string so we trim it down to 5 
-    $security_code = substr($md5_hash, 15, 5); 
+	//Let's generate a totally random string using md5
+	$md5_hash = md5(rand(0,999)); 
 
-    //Set the session to store the security code
-    $_SESSION["klj_sec_code"] = $security_code;
+	//I think the bad things in captcha is two things, O and 0 , so let's remove zero.
+	$security_code = str_replace('0', '', $md5_hash);
 
-    //Set the image width and height
-    $width = 150;
-    $height = 25; 
+	//We don't need a 32 character long string so we trim it down to 5 
+	$security_code = substr($security_code, 15, 5);
 
-    //Create the image resource 
-    $image = ImageCreate($width, $height);  
+	//Set the session to store the security code
+	$_SESSION["klj_sec_code"] = $security_code;
 
-    //We are making three colors, white, black and gray
-    $white = ImageColorAllocate($image, 255, 255, 255);
-    $black = ImageColorAllocate($image, rand(0, 100), 0, rand(0, 50));
-    $grey = ImageColorAllocate($image, 204, 204, 204);
+	//Set the image width and height
+	$width = 150;
+	$height = 25; 
 
-    //Make the background black 
-    ImageFill($image, 0, 0, $black); 
+	//Create the image resource 
+	$image = ImageCreate($width, $height);  
+
+	//We are making three colors, white, black and gray
+	$white = ImageColorAllocate($image, 255, 255, 255);
+	$black = ImageColorAllocate($image, rand(0, 100), 0, rand(0, 50));
+	$grey = ImageColorAllocate($image, 204, 204, 204);
+
+	//Make the background black 
+	ImageFill($image, 0, 0, $black); 
 	
 	//options
 	$x = 10;
 	$y = 14;
 	$angle = rand(-7, -10);
 
-    //Add randomly generated string in white to the image
+	//Add randomly generated string in white to the image
 	if(function_exists('imagettftext'))
 	{
-		imagettftext ($image, 16,$angle , rand(50, $x), $y+rand(1,3), $white,'arial.ttf', $security_code);
+		//
+		// We figure a bug that happens when you add font name without './' before it .. 
+		// he search in the Linux fonts cache , but when you add './' he will know it's our font.
+		//
+		imagettftext ($image, 16,$angle , rand(50, $x), $y+rand(1,3), $white,'./arial.ttf', $security_code);
 	}
 	else
 	{
@@ -70,24 +78,24 @@ function kleeja_cpatcha_image()
 	}
 	
 	//kleeja !
-	imagestring ($image, 1, $width-35, $height-10, 'kleeja', ImageColorAllocate($image, 200, 200, 200));
+	imagestring ($image, 1, $width-35, $height-10, 'Kleeja', ImageColorAllocate($image, 200, 200, 200));
 	
-    //Throw in some lines to make it a little bit harder for any bots to break 
-    ImageRectangle($image,0,0,$width-1,$height-1,$grey); 
-    imageline($image, 0, $height/2, $width, $height/2, $grey); 
-    imageline($image, $width/2, 0, $width/2, $height, $grey); 
+	//Throw in some lines to make it a little bit harder for any bots to break 
+	ImageRectangle($image,0,0,$width-1,$height-1,$grey); 
+	imageline($image, 0, $height/2, $width, $height/2, $grey); 
+	imageline($image, $width/2, 0, $width/2, $height, $grey); 
  
 
 
 			
-    //Tell the browser what kind of file is come in 
-    header("Content-Type: image/png"); 
+	//Tell the browser what kind of file is come in 
+	header("Content-Type: image/png"); 
 
-    //Output the newly created image in jpeg format 
-    ImagePng($image);
+	//Output the newly created image in jpeg format 
+	ImagePng($image);
    
-    //Free up resources
-    ImageDestroy($image);
+	//Free up resources
+	ImageDestroy($image);
 }
 
-?>
+//<--- EOF

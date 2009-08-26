768c40098a434d3a4441fa63952d4cfa8f30f5e2
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2009-08-26 12:23:09 +0000

    improve url fetch function

--- includes/functions.php
+++ includes/functions.php
@@ -661,25 +661,83 @@ function send_mail($to, $body, $subject, $fromaddress, $fromname,$bcc='')
 
 /*
 * get remote files
-* parameters : url : link of file
+* (c) punbb
+* parameters : 
+	url : link of file
+	save_in : folder
 */
-function fetch_remote_file($url, $save_in = false, $timeout = 20)
+function fetch_remote_file($url, $save_in = false, $timeout = 20, $head_only = false, $max_redirects = 10)
 {
 	($hook = kleeja_run_hook('kleeja_fetch_remote_file_func')) ? eval($hook) : null; //run hook
 
-	
-	if(function_exists("curl_init"))
+	// Quite unlikely that this will be allowed on a shared host, but it can't hurt
+	if (function_exists('ini_set'))
+		@ini_set('default_socket_timeout', $timeout);
+	$allow_url_fopen = function_exists('ini_get') ? strtolower(@ini_get('allow_url_fopen')) : strtolower(@get_cfg_var('allow_url_fopen'));
+
+	if(function_exists('curl_init'))
 	{
-		$ch = @curl_init();
-		@curl_setopt($ch, CURLOPT_URL, $url);
-		@curl_setopt($ch, CURLOPT_HEADER, 0);
-		@curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-		@curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+		@curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_NOBODY, $head_only);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0; Kleeja)');
+		
+		// Grab the page
 		$data = @curl_exec($ch);
-		@curl_close($ch);
-		return $data;
+		$responce_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		// Process 301/302 redirect
+		if ($data !== false && ($responce_code == '301' || $responce_code == '302') && $max_redirects > 0)
+		{
+			$headers = explode("\r\n", trim($data));
+			foreach ($headers as $header)
+			{
+				if (substr($header, 0, 10) == 'Location: ')
+				{
+					$responce = fetch_remote_file(substr($header, 10), $save_in, $timeout, $head_only, $max_redirects - 1);
+					if ($head_only)
+					{
+						if($responce != false)
+						{
+							$headers[] = $responce;
+						}
+						return $headers;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+		}
+
+		// Ignore everything except a 200 response code
+		if ($data !== false && $responce_code == '200')
+		{
+			if ($head_only)
+			{
+				return explode("\r\n", str_replace("\r\n\r\n", "\r\n", trim($data)));
+			}
+			else
+			{
+				preg_match('#HTTP/1.[01] 200 OK#', $data, $match, PREG_OFFSET_CAPTURE);
+				$last_content = substr($data, $match[0][1]);
+				$content_start = strpos($last_content, "\r\n\r\n");
+				if ($content_start !== false)
+				{
+					return substr($last_content, $content_start + 4);
+				}
+			}
+		}
+		//<--
 	}
-	else if(function_exists("fsockopen"))
+	// fsockopen() is the second best thing
+	else if(function_exists('fsockopen'))
 	{
 	    $url_parsed = parse_url($url);
 	    $host = $url_parsed['host'];
@@ -691,57 +749,122 @@ function fetch_remote_file($url, $save_in = false, $timeout = 20)
 			$path .= '?' . $url_parsed['query'];
 		}
 	
-	    $out = "GET $path HTTP/1.1\r\nHost: $host\r\nConnection: Close\r\n\r\n";
-
 	    if(!$fp = @fsockopen($host, $port, $errno, $errstr, $timeout))
 		{
 			return false;
 		}
 		
-		@stream_set_timeout($fp, $timeout, 0);	
-		@fwrite($fp, $out);
-	    $body = false;
+		// Send a standard HTTP 1.0 request for the page
+		fwrite($fp, ($head_only ? 'HEAD' : 'GET') . " $path HTTP/1.0\r\n");
+		fwrite($fp, "Host: $host\r\n");
+		fwrite($fp, 'User-Agent: Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0; Kleeja)' . "\r\n");
+		fwrite($fp, 'Connection: Close'."\r\n\r\n");
+			
+		stream_set_timeout($fp, $timeout);
+		$stream_meta = stream_get_meta_data($fp);
+		
+		//let's open new file to save it in.
 		if($save_in)
 		{
 			$fp2 = @fopen($save_in, "w");
 		}
 		
-		$in = '';
-		while (!@feof($fp))
+		// Fetch the response 1024 bytes at a time and watch out for a timeout
+		$in = false;
+		while (!feof($fp) && !$stream_meta['timed_out'])
 		{
-	        $s = @fgets($fp, 1024);
-	        echo $s;
-	        if ($body)
+			$in .= fgets($fp, 1024);
+			if($save_in)
 			{
-				if($save_in)
-				{
-					@fwrite($fp2, $s);
-				}
-				else
-				{
-					$in .= $s;
-				}
+				@fwrite($fp2, $in);
 			}
 			
-			if ($s == "\r\n")
-			{
-				$body = true;
-			}
-	    }
-	    @fclose($fp);
-		
+			$stream_meta = stream_get_meta_data($fp);
+		}
+
+		fclose($fp);
+
 		if($save_in)
 		{
+			unset($in);
 			@fclose($fp2);
 			return true;
 		}
 		
+		// Process 301/302 redirect
+		if ($in !== false && $max_redirects > 0 && preg_match('#^HTTP/1.[01] 30[12]#', $in))
+		{
+			$headers = explode("\r\n", trim($in));
+			foreach ($headers as $header)
+			{
+				if (substr($header, 0, 10) == 'Location: ')
+				{
+					$responce = get_remote_file(substr($header, 10), $save_in, $timeout, $head_only, $max_redirects - 1);
+					if ($responce != false)
+					{
+						$headers[] = $responce;
+					}
+					return $headers;
+				}
+			}
+		}
+		
+		// Ignore everything except a 200 response code
+		if ($in !== false && preg_match('#^HTTP/1.[01] 200 OK#', $in))
+		{
+			if ($head_only)
+			{
+				return explode("\r\n", trim($in));
+			}	
+			else
+			{
+				$content_start = strpos($in, "\r\n\r\n");
+				if ($content_start !== false)
+				{
+					return substr($in, $content_start + 4);
+				}
+			}
+		}
 		return $in;
 	}
+	// Last case scenario, we use file_get_contents provided allow_url_fopen is enabled (any non 200 response results in a failure)
+	else if (in_array($allow_url_fopen, array('on', 'true', '1')))
+	{
+		// PHP5's version of file_get_contents() supports stream options
+		if (version_compare(PHP_VERSION, '5.0.0', '>='))
+		{
+			// Setup a stream context
+			$stream_context = stream_context_create(
+				array(
+					'http' => array(
+						'method'		=> $head_only ? 'HEAD' : 'GET',
+						'user_agent'	=> 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0; Kleeja)',
+						'max_redirects'	=> $max_redirects + 1,	// PHP >=5.1.0 only
+						'timeout'		=> $timeout	// PHP >=5.2.1 only
+					)
+				)
+			);
+
+			$content = @file_get_contents($url, false, $stream_context);
+		}
+		else
+		{
+			$content = @file_get_contents($url);
+		}
+
+		// Did we get anything?
+		if ($content !== false)
+		{
+			// Gotta love the fact that $http_response_header just appears in the global scope (*cough* hack! *cough*)
+			if ($head_only)
+			{
+				return $http_response_header;
+			}
+			return $content;
+		}
+	}
 	
-	//there is known issue here with check update ..
-	return implode('', @file($url));
-	
+	return false;
 }
 
 

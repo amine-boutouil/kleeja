5a7221d0a899cc5c4cc37f8b701f7895dd1ed21a
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2009-10-28 23:59:23 +0000

    trying add custom error handler for Kleeja

--- trunk/includes/common.php
+++ trunk/includes/common.php
@@ -75,6 +75,33 @@ if(function_exists('ini_set'))
 /**
 * functions for start
 */
+function kleeja_show_error($errno, $errstr = '', $errfile = '', $errline = '')
+{
+	switch ($errno)
+	{
+		case E_NOTICE: case E_WARNING: case E_USER_WARNING: case E_USER_NOTICE: case E_STRICT: break;
+		default:
+			header('HTTP/1.1 503 Service Temporarily Unavailable');
+			echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">' . "\n<head>\n";
+			echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' . "\n";
+			echo '<title>' . htmlspecialchars($error_title) . '</title>' . "\n" . '<style type="text/css">' . "\n\t";
+			echo '* { margin: 0; padding: 0; }' . "\n\t" . 'body { background: #fff;margin: 0 auto;padding: 50px;width: 767px;}' . "\n\t";
+			echo '.error {color: #333;background:#ffebe8;border: 1px solid #dd3c10; padding: 10px;font-family:tahoma,arial;font-size: 12px;}' . "\n";
+			echo "</style>\n</head>\n<body>\n\t" . '<div class="error">' . "\n\n\t\t<h2>Kleeja error  : </h2><br />" . "\n";
+			echo "\n\t\t<strong> [ " . $errno . ':' . basename($errfile) . ':' . $errline . ' ] </strong><br /><br />' . "\n\t\t" . $errstr . "\n\t";
+			echo "\n\t\t" . '<br /><br /><small>Visit <a href="http://www.kleeja.com/" title="kleeja">Kleeja</a> Website for more details.</small>' . "\n\t";
+			echo "</div>\n</body>\n</html>";
+			global $SQL;
+			if(isset($SQL))
+			{
+				@$SQL->close();
+			}
+			exit;
+		break;
+    }
+}
+set_error_handler('kleeja_show_error');
+
 function stripslashes_our($value)
 {
 	return is_array($value) ? array_map('stripslashes_our', $value) : stripslashes($value);  

3c87f99f47b88919b3c2d6ba8f35e31aaef9db1f
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2010-03-17 19:10:57 +0000

    improving Kleeja storage system ...

---
+++ trunk/install/includes/default_values.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+* @package install
+* @version $Id: install_sqls.php 1187 2009-10-18 23:10:13Z saanina $
+* @copyright (c) 2007 Kleeja.com
+* @license ./docs/license.txt
+*
+*/
+
+// not for directly open
+if (!defined('IN_COMMON'))
+{
+	exit();
+}
+

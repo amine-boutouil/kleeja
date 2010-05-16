651e56d0eb121723f106541b7b2076d9bf1e182f
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2010-05-16 15:04:24 +0000

    French language pack arrived #2, thanks Daniel ...

---
+++ trunk/lang/fr/install.php
@@ -0,0 +1,130 @@
+<?php
+// French language file
+//  By:2lo.in , Email: webmaster@2lo.in 
+
+
+	if(!isset($lang) || !is_array($lang)) $lang = array();
+
+	$lang['INST_INSTALL_WIZARD']					= 'Kleeja assistant d\'installation';
+	$lang['INST_INSTALL_CLEAN_VER']					= 'Nouvelle Installation';
+	$lang['INST_UPDATE_P_VER']						= 'Mise &agrave; jour ';
+	$lang['INST_AGR_LICENSE']						= 'J`accepte les termes et conditions';
+	$lang['INST_NEXT']								= 'Suivant';
+	$lang['INST_PREVIOUS']							= 'retour';
+	$lang['INST_SITE_INFO']							= 'Site Info';
+	$lang['INST_ADMIN_INFO']						= 'Admin Info';
+	$lang['INST_CHANG_CONFIG']						= 'Missing requirements ... &ecirc;tes-vous s&ucirc;r d\'avoir &eacute;diter le fichier config.php.';
+	$lang['INST_CONNCET_ERR']						= 'Connexion impossible ..';
+	$lang['INST_SELECT_ERR']						= 'Acc&egrave;s impossible &agrave; la base';
+	$lang['INST_NO_WRTABLE']						= 'Ecriture impossible dans le r&eacute;pertoire';
+	$lang['INST_GOOD_GO']							= 'Tout semble &ecirc;tre OK .... continuer';
+	$lang['INST_MSGINS']							= 'Bienvenue dans notre service de t&eacute;l&eacute;chargement, Ici vous pouvez t&eacute;l&eacute;charger ce que vous voulez tant que cela ne viole pas nos conditions.';
+	$lang['INST_CRT_CALL']							= 'Table des Commentaires cr&eacute;&eacute;e.';
+	$lang['INST_CRT_ONL']							= 'Table des utilisateurs en ligne cr&eacute;&eacute;e.';
+	$lang['INST_CRT_REPRS']							= 'Table des rapports cr&eacute;&eacute;e.';
+	$lang['INST_CRT_STS']							= 'Table des Statistiques cr&eacute;&eacute;e.';
+	$lang['INST_CRT_USRS']							= 'Table des Utilisateurs cr&eacute;&eacute;e.';
+	$lang['INST_CRT_ADM']							= 'D&eacute;tails Admin cr&eacute;&eacute;.';
+	$lang['INST_CRT_FLS']							= 'Table des Fichiers cr&eacute;&eacute;e.';
+	$lang['INST_CRT_CNF']							= 'Table Configuration cr&eacute;&eacute;e.';
+	$lang['INST_CRT_EXT']							= 'Table des Extensions cr&eacute;&eacute;e.';
+	$lang['INST_CRT_HKS']							= 'Table des Anti-Pirates cr&eacute;&eacute;e';
+	$lang['INST_CRT_LNG']							= 'Table des Langues cr&eacute;&eacute;e';
+	$lang['INST_CRT_LSTS']							= 'Table des Listes cr&eacute;&eacute;e';
+	$lang['INST_CRT_PLG']							= 'Table des Plugins cr&eacute;&eacute;e';
+	$lang['INST_CRT_TPL']							= 'Table des Templates cr&eacute;&eacute;e';
+	$lang['INST_SQL_OK']							= 'Donn&eacute;s SQL ex&eacute;cut&eacute;es ..';
+	$lang['INST_SQL_ERR']							= 'Erreur sur SQL .. ';
+	$lang['INST_FINISH_SQL']						= 'Kleeja a &eacute;t&eacute; install&eacute;e avec succ&egrave;s, S\'il vous pla&icirc;t supprimer le r&eacute;pertoire d\'installation';
+	$lang['INST_FINISH_ERRSQL']						= 'Oops! il y a des probl&egrave;mes. Veuillez recommencer.';
+	$lang['INST_KLEEJADEVELOPERS']					= 'Merci d\'utiliser Kleeja et bonne chance de notre &eacute;quipe de d&eacute;veloppement';
+	$lang['SITENAME']								= 'Titre du site';
+	$lang['SITEURL']								= 'URL du site';
+	$lang['SITEMAIL']								= 'Email du site';
+	$lang['USERNAME']								= 'Pseudo';
+	$lang['PASSWORD']								= 'Mot de passe';
+	$lang['PASSWORD2']								= 'Confirmer';
+	$lang['EMAIL']									= 'Email';
+	$lang['INDEX']									= 'Index';
+	$lang['ADMINCP']								= 'Panneau de Contr&ocirc;le';
+	$lang['DIR']									= 'rtl';
+	$lang['EMPTY_FIELDS']							= 'Certains champs importants ont &eacute;t&eacute; laiss&eacute;s en blanc!';
+	$lang['WRONG_EMAIL']							= 'Adresse Email incorrecte!';
+	//
+	
+	$lang['DB_INFO_NW']								= 'Le fichier config.php est manquant, remplissez les champs ci-dessous et kleeja cr&eacute;era automatiquement le fichier dans le r&eacute;pertoire kleeja.';
+	$lang['DB_INFO']								= 'Le fichier config.php est manquant, remplissez les champs ci-dessous et kleeja cr&eacute;era automatiquement le fichier.';
+	$lang['DB_SERVER']								= 'Host (serveur)';
+	$lang['DB_TYPE']								= 'Type de Database';
+	$lang['DB_TYPE_MYSQL']							= 'MySQL Standard';
+	$lang['DB_TYPE_MYSQLI']							= 'MySQL Am&eacute;lior&eacute;';
+	$lang['DB_USER']								= 'Utilisateur de la base';
+	$lang['DB_PASSWORD']							= 'Mot de passe de la base';
+	$lang['DB_NAME']								= 'Nom de la base';
+	$lang['DB_PREFIX']								= 'Prefixe des Tables';
+	$lang['VALIDATING_FORM_WRONG']					= 'Un champ obligatoire a &eacute;t&eacute; laiss&eacute; en blanc!';
+	$lang['CONFIG_EXISTS']							= 'Config.php introuvable, Continuer...';
+	$lang['INST_SUBMIT_CONFIGOK']					= 'T&eacute;l&eacute;chargez le fichier dans le r&eacute;pertoire principal';
+	$lang['INST_EXPORT']							= 'Exporter  le Fichier';
+	$lang['INST_OTHER_INFO']						= 'Autres info';
+	$lang['URLS_TYPES']								= 'Style d\'URL du fichier';
+	$lang['DEFAULT']								= 'D&eacute;faut';
+	$lang['FILENAME_URL']							= 'Fichiers';
+	$lang['DIRECT_URL']								= 'Liens directs';
+	$lang['LIKE_THIS']								= 'Example : ';
+
+	//
+	$lang['FUNCTIONS_CHECK']						= 'Les fonctions de contr&ocirc;le';
+	$lang['RE_CHECK']								= 'Re-V&eacute;rifiez';
+	$lang['FUNCTION_IS_NOT_EXISTS']					= 'La fonction %s est d&eacute;sactiv&eacute;e.';
+	$lang['FUNCTION_IS_EXISTS']						= 'La fonction %s est activ&eacute;e.';
+	$lang['FUNCTION_DISC_UNLINK']					= 'La fonction Unlink permet de supprimer et mettre &agrave; jour les fichiers du cache.';
+	$lang['FUNCTION_DISC_GD']						= 'La fonction imagecreate truecolor est une fonction de la librairie GD qui est utilis&eacute;e pour cr&eacute;er des vignettes et pour le contr&ocirc;le de vos photos.';
+	$lang['FUNCTION_DISC_FOPEN']					= 'La fonction fopen est utilis&eacute;e pour contr&ocirc;ler les styles et les fichiers dans kleeja.';
+	$lang['FUNCTION_DISC_MUF']						= 'La fonction move_uploaded_file est utilis&eacute;e pour transf&eacute;rer des fichiers (c\'est la plus importante des fonctions du script).';
+	//
+	$lang['ADVICES_CHECK']							= 'V&eacute;rification avanc&eacute;e (Option)';
+	$lang['ADVICES_REGISTER_GLOBALS']				= '<span style="color:red;padding:0 6px;">La fonction register_globals est activ&eacute;e ..!</span><br /> il est recommand&eacute; de la d&eacute;sactiv&eacute;e.';
+	$lang['ADVICES_MAGIC_QUOTES']					= '<span style="color:red;padding:0 6px;">La fonction magic_quotes est activ&eacute;e ..!</span><br /> il est recommand&eacute; de la d&eacute;sactiv&eacute;e.';
+	
+	//UPDATOR
+	$lang['INST_CHOOSE_UPDATE_FILE']				= 'Choisissez le fichier de mise &agrave; jour appropri&eacute;e';
+	$lang['INST_ERR_NO_SELECTED_UPFILE_GOOD']		= 'Fichier de mise inappropri&eacute; ou absent!';
+	$lang['INST_UPDATE_CUR_VER_IS_UP']				= 'Votre version actuelle est r&eacute;cente que cette mise &agrave; jour.';
+	$lang['INST_UPDATE_SELECT_ONTHER_UPDATES']		= 'Retour et s&eacute;lectionnez une autre mise &agrave; jour.';
+	
+	$lang['INST_NOTES_UPDATE']						= 'Notes sur la mise &agrave; jour';
+	$lang['INST_NOTE_RC2_TO_RC3']					= 'Vous devez remplacer tous les fichiers du script, et pas seulement ex&eacute,cuter cet assistant  ...';
+	$lang['INST_NOTE_RC4_TO_RC5']					= 'Vous devez remplacer tous les nouveaux fichiers de script avec les anciens !.';
+	$lang['INST_NOTE_RC5_TO_RC6']					= 'Vous devez remplacer tous les nouveaux fichiers de script avec les anciens !.';
+	$lang['INST_NOTE_RC6_TO_1.0.0']					= 'Vous devez remplacer tous les nouveaux fichiers de script avec les anciens !.';
+	$lang['RC6_1_CNV_CLEAN_NAMES']					= 'Nettoyage de tous les utilisateurs ...';
+	$lang['INST_UPDATE_IS_FINISH']					= 'Installation termin&eacute;e! Vous pouvez maintenant supprimer le r&eacute;pertoire <br /><b>INSTALL</b><br />...';
+	$lang['IN_INFO']								= 'Remplissez les champs ci-dessous si vous souhaitez int&eacute;grer kleeja avec votre script . Ignorez cette &eacute;tape si vous ne souhaitez pas le faire<br /><span style="color:red;">vous devez changer le syst&egrave;me d\'utilisateur cp admin apr&eacute;s l\'installation de kleeja</span>';
+	$lang['IN_PATH']								= 'Chemin du script';
+	$lang['INST_PHP_LESSMIN']						= 'Vous devez avoir PHP %1$s ou suivant pour installer Kleeja, votre version actuelle est la %2$s';
+	$lang['INST_MYSQL_LESSMIN']						= 'Vous devez avoir MySQL %1$s ou suivant pour installer Kleeja, votre version actuelle est la %2$s';
+	$lang['IS_IT_OFFICIAL']							= 'Avez-vous re&ccedil;u votre copie du script sur Kleeja.com (Site Officiel Kleeja) ?';
+	$lang['IS_IT_OFFICIAL_DESC']					= 'Nous recevons beaucoup de plaintes et de questions sur la cause de certains bugs et les probl&egrave;mes qui se produisent dans kleeja. Probablement nous ne pouvons pas voir d\'ou vient les probl&egrave;mes. Apr&eacute;s v&eacute;rification, nous avons constat&eacute; que quelques copies officieusement publi&eacute;es par les &eacute;diteurs ne sont pas fiables .<br /><br /> <fon style="color:#154188;border-bottom:1px dashed #154188;padding:4px 0;"> Alors &ecirc;tes-vous s&ucirc;r que cette copie a &eacute;t&eacute; t&eacute;l&eacute;charg&eacute;e sur le site officiel kleeja ?</font>';
+	$lang['IS_IT_OFFICIAL_YES']						= 'Oui, ma copie est officielle et je l\'ai t&eacute;l&eacute;charg&eacute;e depuis le site officiel ( Kleeja.com )';
+	$lang['IS_IT_OFFICIAL_NO']						= '<a style="color:red" target="_blank" href="http://www.kleeja.com">Non, Je l\'ai t&eacute;l&eacute;charg&eacute; sur un autre site, allez d&egrave;s maintenant le t&eacute;l&eacute;charger &agrave; partir du site officiel Kleeja</a>';
+	$lang['INST_WHAT_IS_KLEEJA_T']					= 'C\'est qoi Kleeja ?';
+	
+	$lang['INST_WHAT_IS_KLEEJA'] = 'Keeja est un script libre, riche en fonctionnalit&eacute;s, pour le transfert de fichiers et d\'images. Kleeja est con&ccedil;u pour aider les webmasters &agrave; fournir un service d\'h&eacute;bergement de fichiers d&eacute;cent sur leurs sites. Kleeja est livr&eacute; avec un code source simple et puissant, aussi avec des templates simples de sorte que vous pouvez facilement personnaliser vos styles ';
+	
+	$lang['INST_SPECIAL_KLEEJA']					= 'Certaines fonctionnalit&eacute;es Kleeja .. !';
+	$lang['INST_WHAT_IS_KLEEJA_ONE']				= 'Kleeja dispose d\'un syst&egrave;me utilisateurs simple et puissant qui peut &eacute;tre facilement int&eacute;gr&eacute; &agrave; de nombreux sites. Kleeja offre un panneau de commande administrateur qui vous permet de contr&ocirc;ler tout votre site. Vous pouvez aussi personnaliser les styles de Kleeja et l\'installation d\'un lot de plugins  ....  <a target="_blank" href="http://www.kleeja.com/about/">plus de details sur le site Kleeja</a>';
+	$lang['YES']									= 'Oui';
+	$lang['NO']										= 'Non';
+	$lang['PASS_X_CHARS']							= 'Vous devez entrer un minimum de %d caract&egrave;res';
+	$lang['PASS_X_TYPE']							= array('Faible', 'Normal', 'Moyen', 'Haut', 'Tr&egarve;s haut');
+	
+	//PLUGINS
+	$lang['PLUGINS_KLEEJA']							= 'Plugins pour Kleeja .. !';
+	$lang['PLUGINS_BUILT_IN']						= 'Ces plugins sont &agrave; l\'origine cr&eacute;&eacute;s pour Kleeja  . pour d\'autres plugins, visiter Kleeja.com';
+	$lang['PLUGINS_NAME']							= 'nom du Plugin';
+	$lang['PLUGINS_VER']							= 'version';
+	$lang['PLUGINS_DES']							= 'description';	
+	$lang['PLUGINS_INSTALLED']						= 'Le Plugin a &eacute;t&eacute; install&eacute; ! .';	
+
+//<-- EOF

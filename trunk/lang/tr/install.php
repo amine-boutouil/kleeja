1cfc892473808c1eb3e9c564f15fbe0114cc2b35
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2010-05-22 00:57:58 +0000

    adds turkish language pack ...

---
+++ trunk/lang/tr/install.php
@@ -0,0 +1,136 @@
+<?php
+// Kleeja Türkçe dil dosyasý
+//  By:CEZALAND || rapper_cezaland@hotmail.com
+// www.ForumVakti.net
+
+
+	if(!isset($lang) || !is_array($lang)) $lang = array();
+
+	$lang['INST_INSTALL_WIZARD']					= 'Kleeja yükleme sihirbazý';
+	$lang['INST_INSTALL_CLEAN_VER']					= 'Yeni kurulum';
+	$lang['INST_UPDATE_P_VER']						= 'Güncelleme ';
+	$lang['INST_AGR_LICENSE']						= 'Kurallarý okudum ve onayladým';
+	$lang['INST_NEXT']								= 'Ileri';
+	$lang['INST_PREVIOUS']							= 'Geri';
+	$lang['INST_SITE_INFO']							= 'Site Ayarlarý';
+	$lang['INST_ADMIN_INFO']						= 'Admin Ayarlarý';
+	$lang['INST_CHANG_CONFIG']						= 'Eksik ayarlar ... config.php dosyasýnýn doðruluðundan emin olun.';
+	$lang['INST_CONNCET_ERR']						= 'Baðlantý Yapýlamadý ..';
+	$lang['INST_SELECT_ERR']						= 'Database seçilmedi';
+	$lang['INST_NO_WRTABLE']						= 'Bu dizin yazýlabilir deðil';
+	$lang['INST_GOOD_GO']							= 'Herþey tamam gibi gözüküyor :) .... Devam edebilirsiniz';
+	$lang['INST_MSGINS']							= 'Dosya yükleme servisimize Hoþgeldiniz, burda dosyalarýnýzý yükleyebilir ve kurallarýmýza uyduðu sürece barýndýrabilirsiniz';
+	$lang['INST_CRT_CALL']							= 'Yorum tablosu oluþturuldu.';
+	$lang['INST_CRT_ONL']							= 'Online kullanýcýlar tablosu oluþturuldu.';
+	$lang['INST_CRT_REPRS']							= 'Raporlar tablosu oluþturuldu.';
+	$lang['INST_CRT_STS']							= 'Istatistikler tablosu oluþturuldu.';
+	$lang['INST_CRT_USRS']							= 'Kullanýcýlar tablosu oluþturuldu.';
+	$lang['INST_CRT_ADM']							= 'Admin detaylarý oluþturuldu.';
+	$lang['INST_CRT_FLS']							= 'Dosyalar tablosu oluþturuldu.';
+	$lang['INST_CRT_CNF']							= 'Ayarlar tablosu oluþturuldu.';
+	$lang['INST_CRT_EXT']							= 'Dosya uzantýlarý tablosu oluþturuldu.';
+	$lang['INST_CRT_HKS']							= 'Hacks tablosu oluþturuldu';
+	$lang['INST_CRT_LNG']							= 'Dil tablosu oluþturuldu';
+	$lang['INST_CRT_LSTS']							= 'Listeler tablosu oluþturuldu';
+	$lang['INST_CRT_PLG']							= 'Eklentiler tablosu oluþturuldu';
+	$lang['INST_CRT_TPL']							= 'Templates tablosu oluþturuldu';
+	$lang['INST_SQL_OK']							= 'SQL Baþarýyla yazýldý ..';
+	$lang['INST_SQL_ERR']							= 'SQL Yazmada hata .. ';
+	$lang['INST_FINISH_SQL']						= 'Kleeja baþarýyla yüklendi, Lütfen INSTALL klasörünü kaldýrýn';
+	$lang['INST_FINISH_ERRSQL']						= 'Oww! hatalar oluþtu gibi gözüküyor, tekrar deneyin.';
+	$lang['INST_KLEEJADEVELOPERS']					= 'Kleeja kullandýðýn için teþekkürler ve bizim tayfamýzdan size iyi þanslar';
+	$lang['SITENAME']								= 'Website Baþlýðý';
+	$lang['SITEURL']								= 'Website URL';
+	$lang['SITEMAIL']								= 'Website Email';
+	$lang['USERNAME']								= 'Kullanýcý Adý';
+	$lang['PASSWORD']								= 'Þifre';
+	$lang['PASSWORD2']								= 'Þifreyi tekrar girin';
+	$lang['EMAIL']									= 'Email';
+	$lang['INDEX']									= 'AnaSayfa';
+	$lang['ADMINCP']								= 'Kontrol Paneli';
+	$lang['DIR']									= 'rtl';
+	$lang['EMPTY_FIELDS']							= 'Bazý önemli alanlarý boþ býraktýn!';
+	$lang['WRONG_EMAIL']							= 'Hatalý email adresi';
+	//
+	
+	$lang['DB_INFO_NW']								= 'Config.php dosyasý kayýp, tüm alanlarý doldurursanýz kleeja size config.php yaratacak.';
+	$lang['DB_INFO']								= 'Config.php dosyasý kayýp, tüm alanlarý doldurursanýz kleeja size config.php yaratacak';
+	$lang['DB_SERVER']								= 'Host';
+	$lang['DB_TYPE']								= 'Database tipi';
+	$lang['DB_TYPE_MYSQL']							= 'MySQL Standard';
+	$lang['DB_TYPE_MYSQLI']							= 'MySQL Improved';
+	$lang['DB_USER']								= 'Database Kullanýcýsý';
+	$lang['DB_PASSWORD']							= 'Database Þifresi';
+	$lang['DB_NAME']								= 'Database Ismi';
+	$lang['DB_PREFIX']								= 'Tablo öneki(opsiyonel)';
+	$lang['VALIDATING_FORM_WRONG']					= 'Gerekli alanlarý boþ býraktýn!';
+	$lang['CONFIG_EXISTS']							= 'Config.php bulundu, Devam edin...';
+	$lang['INST_SUBMIT_CONFIGOK']					= 'Ana dizine yükleyin';
+	$lang['INST_EXPORT']							= 'Dosya çýkart';
+	$lang['INST_OTHER_INFO']						= 'Diðer bilgiler';
+	$lang['URLS_TYPES']								= 'Dosya URL\'sinde yeniden yazma tipi';
+	$lang['DEFAULT']								= 'Default';
+	$lang['FILENAME_URL']							= 'Dosya isimleri';
+	$lang['DIRECT_URL']								= 'Direk linkler';
+	$lang['LIKE_THIS']								= 'Örnek : ';
+
+	//
+	$lang['FUNCTIONS_CHECK']						= 'Fonksiyonlarý kontrol et';
+	$lang['RE_CHECK']								= 'YenidenKontrol et';
+	$lang['FUNCTION_IS_NOT_EXISTS']					= 'Fonksiyon %s kapalý.';
+	$lang['FUNCTION_IS_EXISTS']						= 'Fonksiyon %s açýk.';
+	$lang['FUNCTION_DISC_UNLINK']					= 'Fonksiyon Unlink çerez dosyalarýný temizlemeye yarar.';
+	$lang['FUNCTION_DISC_GD']						= 'Fonksiyon imagecreatetruecolor Fotoðraflarý kontrol etmeye, önizlemeler yaratmaya yarar.';
+	$lang['FUNCTION_DISC_FOPEN']					= 'Fonksiyon fopen Kleeja\'nýn içindeki stilleri & Dosyalarý kontrol eder.';
+	$lang['FUNCTION_DISC_MUF']						= 'Fonksiyon move_uploaded_file Bu fonksiyon dosyalarý yüklemeye yarar ve script için önemlidir.';
+	//
+	$lang['ADVICES_CHECK']							= 'Geliþmiþ kontrol (Opsiyonel)';
+	$lang['ADVICES_REGISTER_GLOBALS']				= '<span style="color:red;padding:0 6px;">register_globals fonksiyonu açýk ..!</span><br /> Bunu kapatmanýz gerek.';
+	$lang['ADVICES_MAGIC_QUOTES']					= '<span style="color:red;padding:0 6px;">magic_quotes fonksiyonu açýk ..!</span><br /> Bunu kapatmanýz gerek.';
+	
+	// 18 Mayýs 2010 saat 17:05 Çeviri iþlemi install.php üzerinde bitmek üzere...
+	//UPDATOR
+	$lang['INST_CHOOSE_UPDATE_FILE']				= 'Yükseltme dosyasýný seçin';
+	$lang['INST_ERR_NO_SELECTED_UPFILE_GOOD']		= 'hatalý yükseltme dosyasý, yada bulunamýyor!';
+	$lang['INST_UPDATE_CUR_VER_IS_UP']				= 'Þuanki versiyonunuz yükseltmek için seçtiðinizden daha yeni.';
+	$lang['INST_UPDATE_SELECT_ONTHER_UPDATES']		= 'Geri dön ve baþka yükseltme seç.';
+	
+	$lang['INST_NOTES_UPDATE']						= 'Yükseltme Notlarý';
+	$lang['INST_NOTE_RC2_TO_RC3']					= 'Sihirbazý çalýþtýrmadan önce dosyalarý deðiþtirmelisin  ...';
+	$lang['INST_NOTE_RC4_TO_RC5']					= 'Yeni script dosyalarý ile eskilerini deðiþtirmeye ihtiyacýn var !.';
+	$lang['INST_NOTE_RC5_TO_RC6']					= 'Yeni script dosyalarý ile eskilerini deðiþtirmeye ihtiyacýn var !.';
+	$lang['INST_NOTE_RC6_TO_1.0.0']					= 'Yeni script dosyalarý ile eskilerini deðiþtirmeye ihtiyacýn var !.';
+	$lang['RC6_1_CNV_CLEAN_NAMES']					= 'Kullanýcý isimleri temizlenmeli ...';
+	// 18 MAYIS 2010 16:32 devam ediyoruz...
+	$lang['INST_UPDATE_IS_FINISH']					= 'Yükleme iþlemi baþarýyla tamamlandý. Güvenliðiniz için siteye giriþ yapmadan önce <br /><b>INSTALL</b><br /> klasörüne silin...';
+	$lang['IN_INFO']								= 'Fill in the fields below if you want to integrate kleeja with your script . Ignore this step if you do not wish to do it<br /><span style="color:red;">you should change user system from admin cp after installing kleeja</span>';
+	$lang['IN_PATH']								= 'Path of script';
+	$lang['INST_PHP_LESSMIN']						= 'Kleeja\'yý yükleyebilmek için PHP %1$s yada daha yüksek versiyonuna ihtiyacýnýz var, þu anki versiyon:  %2$s';
+	$lang['INST_MYSQL_LESSMIN']						= 'Kleeja\'yý yükleyebilmek için MYSQL %1$s versiyonuna ihtiyacýnýz var, þu anki versiyon: %2$s';
+	$lang['IS_IT_OFFICIAL']							= 'Sizdeki Kleeja dosyalarýný nereden buldunuz? ?';
+	$lang['IS_IT_OFFICIAL_DESC']					= 'Eðer dosyalarý bizim sitemizden indirirseniz kleeja hakkýnda her türlü konuda bizden yardým alabilirsiniz fakat Baþka sitelerden aldýðýnýzda yardým ve destek hizmetini vermiyoruz . Ayrýca bulduðunuz baþka sitelerden olan dosyalar güvenilir deðildir .<br /><br /> <fon style="color:#154188;border-bottom:1px dashed #154188;padding:4px 0;">Bunun için neden Kleeja\'yý neden Kleeja\'nýn kendi sitesinden indirmiyorsun?</font>';
+	$lang['IS_IT_OFFICIAL_YES']						= 'Kleeja\'nýn kendi sitesinden Kleeja.com';
+	$lang['IS_IT_OFFICIAL_NO']						= '<a style="color:red" target="_blank" href="http://www.kleeja.com">Hayýr, Ben Kleeja\'yý baþka siteden indirdim, Týklayýn ve Kleeja\'nýn kendi sitesinden indirin.</a>';
+	$lang['INST_WHAT_IS_KLEEJA_T']					= 'Kleeja Nedir?';
+// Tarih: 18 Mayýs 2010 saat 16:22 CEZALAND Türkçeleþtirmeye devam ediyor... Emeðe saygý burdaki yazýnýn size hiçbir zararý yok burda kalsa biyerinizmi eksilcek =)	
+// Türkçeleþtirme kafama göre devam ediyorum o yüzden her bir satýrýn saat farklý =)
+// Bir teþekkür yeter...
+	$lang['INST_WHAT_IS_KLEEJA'] = 'Kleeja ücretsiz, zengin özellikli,  dosya ve resim yükleme scriptidir. Kleeja webmasterlara yardýmcý olabilmek için yapýldý. Webmasterlar kendi dosya yükleme sitesini kurup rahatça kullanýcaklar ve diðer sitelerdeki gibi bekleme derdi olmuyacak . Kleeja tek kaynak kodu ve Kaliteli üyelik sistemi ile geliyor , ve sizin kendi stilinizi yaratmanýza imkan tanýyan bir sistemle kodlandý. Böylelikle istediðiniz gibi þekillendirebilirsiniz. ';
+	
+	$lang['INST_SPECIAL_KLEEJA']					= 'Bazý Kleeja özellikleri .. !';
+	$lang['INST_WHAT_IS_KLEEJA_ONE']				= 'Kleeja kaliteli üyelik sistemi ile birçok forum sistemi ile birleþebilmekte!! Bunu biliyormuydunuz? . Kleejanýnda çok güzel bir admin kontrol paneli var hemde birçok admin panelinden daha güzel ve daha kullanýþlý. Buradan sitenizde deðiþtirmek istediðiniz herþeyi deðiþtirebilirsiniz , hatta kendi stilinizi burdan düzenlemenize bile html editör ile destek veriyor . Ayrýca Kleeja\'nýn sitesindede size özel hazýrlanmýþ stiller ve eklentiler bulabilirsiniz  ....  <a target="_blank" href="http://www.kleeja.com/about/">Kleeja sitesinde daha fazla detay bul </a>';
+	$lang['YES']									= 'Evet';
+	$lang['NO']										= 'Hayýr';
+	$lang['PASS_X_CHARS']							= 'En az %d karakter girmelisiniz';
+	$lang['PASS_X_TYPE']							= array('Zayýf', 'Normal', 'Orta', 'Saðlam', 'Çok saðlam');
+	
+	//PLUGINS
+	$lang['PLUGINS_KLEEJA']							= 'Kleeja için Eklentiler .. !';
+	$lang['PLUGINS_BUILT_IN']						= 'Burdaki eklentiler kleejanýn orjinal eklentileri  . Dahasý için Kleeja.com u ziyaret edin';
+	$lang['PLUGINS_NAME']							= 'Eklenti ismi';
+	$lang['PLUGINS_VER']							= 'versiyon';
+	$lang['PLUGINS_DES']							= 'açýklamma';	
+	$lang['PLUGINS_INSTALLED']						= 'Eklentiler baþarýyla yüklendi ! .';	
+
+//<-- 18 Mayýs 2010 saat 17:09 install.php üzerindeki Türkçeleþtirme çalýþmalarý bu saat itibarýyla bitmiþ bulunmakta
+// CEZALAND || Emeðe saygý ...

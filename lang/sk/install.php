1a9546c83980bf6652b349fb56114bd0aa4716f8
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2010-07-04 14:03:56 +0000

    add slovak language ... thanks for Emsit #2

---
+++ lang/sk/install.php
@@ -0,0 +1,134 @@
+<?php
+//
+// kleeja language file, install
+// Slovak
+// Translated By:emsit , Contact: www.emsit.info , Website: www.emsit.sk
+//
+
+
+	if(!isset($lang) || !is_array($lang)) $lang = array();
+
+	$lang['DIR']									= 'ltr';
+	$lang['INST_INSTALL_WIZARD']					= 'Kleeja sprievodca inštaláciou';
+	$lang['INST_INSTALL_CLEAN_VER']					= 'Nová inštalácia';
+	$lang['INST_UPDATE_P_VER']						= 'Aktualizácia';
+	$lang['INST_AGR_LICENSE']						= 'Súhlasím s podmienkami';
+	$lang['INST_NEXT']								= 'Ďalej';
+	$lang['INST_PREVIOUS']							= 'Spät';
+	$lang['INST_SITE_INFO']							= 'Informácie o web stránke';
+	$lang['INST_ADMIN_INFO']						= 'Administrátorské informácie';
+	$lang['INST_CHANG_CONFIG']						= 'Chýbajúce požiadavky.. Uistite sa, že ste editovali súbor config.php.';
+	$lang['INST_CONNCET_ERR']						= 'Nedá sa pripojiť..';
+	$lang['INST_SELECT_ERR']						= 'Nedá sa vybrať databáza';
+	$lang['INST_NO_WRTABLE']						= 'Adresár nie je zapisovateľný';
+	$lang['INST_GOOD_GO']							= 'Všetko sa zdá byť OK..Pokračujte ďalej';
+	$lang['INST_MSGINS']							= 'Vitajte v našej službe určenej na ukladanie a zdielanie obrázkov a súborov.';
+	$lang['INST_CRT_CALL']							= 'Tabuľka komentárov bola vytvorená.';
+	$lang['INST_CRT_ONL']							= 'Tabuľka online užívateľov bola vytvorená.';
+	$lang['INST_CRT_REPRS']							= 'Tabuľka hlásení bola vytvorená.';
+	$lang['INST_CRT_STS']							= 'Tabuľka štatistík bola vytvorená.';
+	$lang['INST_CRT_USRS']							= 'Tabuľka užívateľov bola vytvorená.';
+	$lang['INST_CRT_ADM']							= 'Detaily administrátora boli zapísané.';
+	$lang['INST_CRT_FLS']							= 'Tabuľka súborov bola vytvorená.';
+	$lang['INST_CRT_CNF']							= 'Tabuľka nastavení bola vytvorená.';
+	$lang['INST_CRT_EXT']							= 'Tabuľka typov súborov bola vytvorená.';
+	$lang['INST_CRT_HKS']							= 'Tabuľka hacks bola vytvorená.';
+	$lang['INST_CRT_LNG']							= 'Tabuľka jazykov bola vytvorená.';
+	$lang['INST_CRT_LSTS']							= 'Tabuľka zoznamov bola vytvorená.';
+	$lang['INST_CRT_PLG']							= 'Tabuľka rozšírení bola vytvorená.';
+	$lang['INST_CRT_TPL']							= 'Tabuľka šablóny bola vytvorená.';
+	$lang['INST_SQL_OK']							= 'Vykonanie SQL bolo úspešné..';
+	$lang['INST_SQL_ERR']							= 'Chyba pri spúšťaní SQL.. ';
+	$lang['INST_FINISH_SQL']						= 'Kleeja bola úspešne nainštalovaná, prosím odstráňte inštalačný adresár.';
+	$lang['INST_FINISH_ERRSQL']						= 'Zdá sa, že nastal problém, skúste to znova.';
+	$lang['INST_KLEEJADEVELOPERS']					= 'Ďakujeme, že používate Kleeja.';
+	$lang['SITENAME']								= 'Názov web stránky';
+	$lang['SITEURL']								= 'URL web stránky';
+	$lang['SITEMAIL']								= 'E-mail web stránky';
+	$lang['USERNAME']								= 'Užívateľské meno';
+	$lang['PASSWORD']								= 'Heslo';
+	$lang['PASSWORD2']								= 'Potvrdiť heslo';
+	$lang['EMAIL']									= 'E-mail';
+	$lang['INDEX']									= 'Domov';
+	$lang['ADMINCP']								= 'Ovládací panel';
+  //	$lang['DIR']									= 'rtl';
+	$lang['EMPTY_FIELDS']							= 'Niektoré dôležité polia ostali nevyplnené!';
+	$lang['WRONG_EMAIL']							= 'Nesprávna e-mailová adresa!';
+	//
+	
+	$lang['DB_INFO_NW']								= 'Chýba súbor config.php, vyplňte polia nižšie a Kleeja vytvorí súbor automaticky v adresári Kleeja.';
+	$lang['DB_INFO']								= 'Chýba súbor config.php, vyplňte polia nižšie a Kleeja vytvorí súbor automaticky.';
+	$lang['DB_SERVER']								= 'Server MySQL';
+	$lang['DB_TYPE']								= 'Typ databázy';
+	$lang['DB_TYPE_MYSQL']							= 'Štandardná MySQL';
+	$lang['DB_TYPE_MYSQLI']							= 'Vylepšená MySQL';
+	$lang['DB_USER']								= 'Prihlasovacie meno pre databázu';
+	$lang['DB_PASSWORD']							= 'Heslo pre databázu';
+	$lang['DB_NAME']								= 'Meno databázy';
+	$lang['DB_PREFIX']								= 'Predpona tabuliek';
+	$lang['VALIDATING_FORM_WRONG']					= 'Povinné pole zostalo prázdne!';
+	$lang['CONFIG_EXISTS']							= 'Config.php bol nájdený. Pokračujte ďalej..';
+	$lang['INST_SUBMIT_CONFIGOK']					= 'Nahranie súborov do hlavného adresára';
+	$lang['INST_EXPORT']							= 'Export súboru';
+	$lang['INST_OTHER_INFO']						= 'Ostatné informácie';
+	$lang['URLS_TYPES']								= 'Štýl URL odkazov';
+	$lang['DEFAULT']								= 'Predvolený';
+	$lang['FILENAME_URL']							= 'Názvy súborov';
+	$lang['DIRECT_URL']								= 'Priame odkazy';
+	$lang['LIKE_THIS']								= 'Príklad ako bude vyzerať odkaz : ';
+
+	//
+	$lang['FUNCTIONS_CHECK']						= 'Funkcia kontroly';
+	$lang['RE_CHECK']								= 'Znova skontrolovať';
+	$lang['FUNCTION_IS_NOT_EXISTS']					= 'Funkci %s je vypnutá.';
+	$lang['FUNCTION_IS_EXISTS']						= 'Funkcia %s je zapnutá.';
+	$lang['FUNCTION_DISC_UNLINK']					= 'Funkcia Unlink sa používa na zneškodnenie a aktualizáciu Cache súborov.';
+	$lang['FUNCTION_DISC_GD']						= 'Funkcia imagecreatetruecolor je funkcia knižnice GD, ktorá je použitá na vytvorenie náhľadov obrázkov.';
+	$lang['FUNCTION_DISC_FOPEN']					= 'Funkcia fopen sa používa na ovládanie štýlov a obrázkov v kleeja.';
+	$lang['FUNCTION_DISC_MUF']						= 'Funkcia move_uploaded_file sa používa na ukladanie súborov a je najdôležitejšiou funkciu v skripte.';
+	//
+	$lang['ADVICES_CHECK']							= 'Pokročilá kontrola (Voliteľné)';
+	$lang['ADVICES_REGISTER_GLOBALS']				= '<span style="color:red;padding:0 6px;">Funkcia register_globals je zapnutá!</span><br />Odporúčame ju vypnúť.';
+	$lang['ADVICES_MAGIC_QUOTES']					= '<span style="color:red;padding:0 6px;">Funkcia magic_quotes je zapnutá!</span><br /> Odporúčame ju vypnúť.';
+	
+	//UPDATOR
+	$lang['INST_CHOOSE_UPDATE_FILE']				= 'Vyberte si príslušnú aktualizáciu súboru';
+	$lang['INST_ERR_NO_SELECTED_UPFILE_GOOD']		= 'Nevhodný aktualizačný súbor, alebo nastala chýba!';
+	$lang['INST_UPDATE_CUR_VER_IS_UP']				= 'Vaša aktuálna verzia je novšia než táto aktualizácia.';
+	$lang['INST_UPDATE_SELECT_ONTHER_UPDATES']		= 'Vráťte sa späť a vyberte inú aktualizáciu.';
+	
+	$lang['INST_NOTES_UPDATE']						= 'Poznámky aktualizácie';
+	$lang['INST_NOTE_RC2_TO_RC3']					= 'Musíte nahradiť všetky súbory skriptov, a nie len spustiť tohto sprievodcu..';
+	$lang['INST_NOTE_RC4_TO_RC5']					= 'Musíte nahradiť všetky staré súbory skriptov za nové!';
+	$lang['INST_NOTE_RC5_TO_RC6']					= 'Musíte nahradiť všetky staré súbory skriptov za nové!';
+	$lang['INST_NOTE_RC6_TO_1.0.0']					= 'Musíte nahradiť všetky staré súbory skriptov za nové!';
+	$lang['RC6_1_CNV_CLEAN_NAMES']					= 'Užívateľské mená boly upratané..';
+	$lang['INST_UPDATE_IS_FINISH']					= 'Inštalácia dokončená! Teraz môžete zmazať adresár <br /><b>INSTALL</b><br />';
+	$lang['IN_INFO']								= 'Vyplňte pole nižšie, ak chcete integrovať Kleeja s Vaším skriptom. Tento krok môžete ignorovať. <br /><span style="color:red;">Užívateľsky systém môžete zmeniť neskor v ovládacom panely.</span>';
+	$lang['IN_PATH']								= 'Cesta skriptu';
+	$lang['INST_PHP_LESSMIN']						= 'Potrebujete PHP verziu %1$s alebo vyššiu pre inštaláciu Kleeja, aktuálna verzia je %2$s';
+	$lang['INST_MYSQL_LESSMIN']						= 'Potrebujete MySQL verziu %1$s alebo vyššiu pre inštaláciu Kleeja, aktuálna verzia je %2$s';
+	$lang['IS_IT_OFFICIAL']							= 'Prevzali ste Vašu kópiu Kleeja z oficiálnej web stránky (kleeja.com)?';
+	$lang['IS_IT_OFFICIAL_DESC']					= 'Dostávame veľa sťažností a otázok týkajúcich sa príčin niektorých chýb a problémov, ktoré sa vyskytujú v Kleeja. Po tom čo sme ich prešetrili, sme zistili že sa vyskytujú len v niektorých neoficiálnych kópiách ktoré pochádzajú z nedôveryhodného zdroja. <br /><br /> <fon style="color:#154188;border-bottom:1px dashed #154188;padding:4px 0;"> Takže ste si istí, že táto kópie je stiahnutá z oficiálnych stránok kleeja?</font>';
+	$lang['IS_IT_OFFICIAL_YES']						= 'Áno, moja kópia je oficiálna a je stiahnutá z oficiálnej web stránky ( Kleeja.com )';
+	$lang['IS_IT_OFFICIAL_NO']						= '<a style="color:red" target="_blank" href="http://www.kleeja.com">Nie, stiahol som si kópiu Kleeja z iného zdroja, pokračujte teraz a stiahnite si ju z Kleeja oficiálnej web stránky.</a>';
+	$lang['INST_WHAT_IS_KLEEJA_T']					= 'Čo je Kleeja?';
+	
+	$lang['INST_WHAT_IS_KLEEJA'] = 'Projekt Keeja je zadarmo, obsahuje bohatý systém na nahrávanie súborov a obrázkov. Kleeja pomáha webmastrom poskytnúť na svojich stránkach prepracovaný systém na nahrávanie a zdieľanie súborov. Prichádza s jednoduchým zdrojovým kódom a výkonným užívateľským systémom, tiež môžete ľahko upravovať svoje šablóny.';
+	
+	$lang['INST_SPECIAL_KLEEJA']					= 'Niektoré funkcie Kleeja..';
+	$lang['INST_WHAT_IS_KLEEJA_ONE']				= 'Kleeja ma jednoduchý a výkonný užívateľský systém, ktorý možno ľahko integrovať s mnohými systémami na príklad phpBB. Poskytuje jednoduchý ovládací panel, vďaka ktorému budete mať kontrolu nad svojou web stránkou. Tiež si môžete prispôsobiť Kleeja vďaka šablónam a rozšíreniam..  <a target="_blank" href="http://www.kleeja.com/about/">viac informácií na Kleeja web stránke</a>';
+	$lang['YES']									= 'Áno';
+	$lang['NO']										= 'Nie';
+	$lang['PASS_X_CHARS']							= 'Musíte zadať aspoň %d znakov';
+	$lang['PASS_X_TYPE']							= array('Slabé', 'Normálne', 'Stredne silné', 'Silné', 'Veľmi silné');
+	
+	//PLUGINS
+	$lang['PLUGINS_KLEEJA']							= 'Rozšírenia pre Kleeja..';
+	$lang['PLUGINS_BUILT_IN']						= 'Tieto rozšírenia boli vyrobene v originálnom systéme Kleeja. Pre viac rozšírení navštívte oficiálnu web stránku kleeja.com';
+	$lang['PLUGINS_NAME']							= 'Meno rozšírenia';
+	$lang['PLUGINS_VER']							= 'Verzia';
+	$lang['PLUGINS_DES']							= 'popis';	
+	$lang['PLUGINS_INSTALLED']						= 'Rozšírenie bolo úspešné nainštalované.';	
+
+//<-- EOF

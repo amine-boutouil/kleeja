193ca0049d583ff79ec31c567faa6b0d403fad5d
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2010-10-28 00:50:04 +0000

    fix phpBB auth integration

--- trunk/includes/auth_integration/phpbb.php
+++ trunk/includes/auth_integration/phpbb.php
@@ -46,6 +46,16 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 			$forum_user	= $dbuser;
 			$forum_pass	= $dbpasswd;
 			$forum_prefix = $table_prefix;
+
+			if(empty($dbhost))
+			{
+				$forum_srv = 'localhost';
+			}
+
+			if(!empty($dbport))
+			{
+				$forum_srv .= ':' . $dbport;
+			}
 		}
 		else
 		{
@@ -72,7 +82,7 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 	//conecting ...		
 	$SQLBB	= new SSQL($forum_srv, $forum_user, $forum_pass, $forum_db, true);
 
-	$SQLBB->set_names('UTF-8');
+	$SQLBB->set_names('utf8');
 
 	unset($forum_pass); // We do not need this any longer
 
@@ -90,7 +100,7 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 					'FROM'		=> "`{$forum_prefix}users`",
 				);
 	
-	$query2['WHERE'] = $hashed ?  "user_id=" . intval($name) . "  AND user_password='" . $SQLBB->real_escape($pass) . "' " : "username_clean='" . utf8_clean_string($name) . "'";
+	$query2['WHERE'] = $hashed ?  "user_id=" . intval($name) . "  AND user_password='" . $SQLBB->real_escape($pass) . "' " : "username_clean='" .  $SQLBB->real_escape(utf8_clean_string($name)) . "'";
 		
 	if($return_name)
 	{
@@ -105,6 +115,8 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 		$result2 = $SQLBB->build($query2);					
 		while($row=$SQLBB->fetch_array($result2))
 		{
+			$SQLBB->freeresult($result2); 
+
 			if($return_name)
 			{
 				return $row['username'];
@@ -117,7 +129,6 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 				}
 			}
 		}
-			$SQLBB->freeresult($result2);   
 	}
 	else
 	{
@@ -129,7 +140,7 @@ function kleeja_auth_login ($name, $pass, $hashed = false, $expire, $loginadm = 
 		$SQLBB->close();
 		return false;
 	}
-
+	
 	($hook = kleeja_run_hook('qr_select_usrdata_phpbb_usr_class')) ? eval($hook) : null; //run hook		
 	$result = $SQLBB->build($query);
 
@@ -199,54 +210,110 @@ function phpbb_check_hash($password, $hash)
 {
 	$itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 	if (strlen($hash) == 34)
+	{
 		return (_hash_crypt_private($password, $hash, $itoa64) === $hash) ? true : false;
+	}
+
 	return (md5($password) === $hash) ? true : false;
 }
+
+/**
+* Generate salt for hash generation
+*/
 function _hash_gensalt_private($input, &$itoa64, $iteration_count_log2 = 6)
 {
 	if ($iteration_count_log2 < 4 || $iteration_count_log2 > 31)
+	{
 		$iteration_count_log2 = 8;
+	}
+
 	$output = '$H$';
 	$output .= $itoa64[min($iteration_count_log2 + ((PHP_VERSION >= 5) ? 5 : 3), 30)];
 	$output .= _hash_encode64($input, 6, $itoa64);
+
 	return $output;
 }
+
+/**
+* Encode hash
+*/
 function _hash_encode64($input, $count, &$itoa64)
 {
 	$output = '';
 	$i = 0;
+
 	do
 	{
 		$value = ord($input[$i++]);
 		$output .= $itoa64[$value & 0x3f];
+
 		if ($i < $count)
+		{
 			$value |= ord($input[$i]) << 8;
+		}
+
 		$output .= $itoa64[($value >> 6) & 0x3f];
+
 		if ($i++ >= $count)
+		{
 			break;
+		}
+
 		if ($i < $count)
+		{
 			$value |= ord($input[$i]) << 16;
+		}
+
 		$output .= $itoa64[($value >> 12) & 0x3f];
+
 		if ($i++ >= $count)
+		{
 			break;
+		}
+
 		$output .= $itoa64[($value >> 18) & 0x3f];
 	}
 	while ($i < $count);
+
 	return $output;
 }
+
+/**
+* The crypt function/replacement
+*/
 function _hash_crypt_private($password, $setting, &$itoa64)
 {
 	$output = '*';
+
+	// Check for correct hash
 	if (substr($setting, 0, 3) != '$H$')
+	{
 		return $output;
+	}
+
 	$count_log2 = strpos($itoa64, $setting[3]);
+
 	if ($count_log2 < 7 || $count_log2 > 30)
+	{
 		return $output;
+	}
+
 	$count = 1 << $count_log2;
 	$salt = substr($setting, 4, 8);
-	if (strlen($salt) != 8)
-		return $output;
 
+	if (strlen($salt) != 8)
+	{
+		return $output;
+	}
+
+	/**
+	* We're kind of forced to use MD5 here since it's the only
+	* cryptographic primitive available in all versions of PHP
+	* currently in use.  To implement our own low-level crypto
+	* in PHP would result in much worse performance and
+	* consequently in lower iteration counts and hashes that are
+	* quicker to crack (by non-PHP code).
+	*/
 	if (PHP_VERSION >= 5)
 	{
 		$hash = md5($salt . $password, true);
@@ -265,7 +332,9 @@ function _hash_crypt_private($password, $setting, &$itoa64)
 		}
 		while (--$count);
 	}
+
 	$output = substr($setting, 0, 12);
 	$output .= _hash_encode64($hash, 16, $itoa64);
+
 	return $output;
 }

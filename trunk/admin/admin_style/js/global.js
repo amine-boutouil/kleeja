2a1e7145ccba4cc916868a93c1bd2e7688a1c7a8
altar3q <altar3q@940b39b9-983a-0410-8f00-47514fe486d8>
2011-02-06 20:10:38 +0000

    Updating style Control Panel

---
+++ trunk/admin/admin_style/js/global.js
@@ -0,0 +1,46 @@
+/** ------------------------------------------------
+	^_^ i love jquery
+	package:	Kleeja  copyright(c)2007 (http://www.kleeja.com)
+	-------------------------------------------------
+	$Id: global.js 1537 2010-07-23 01:39:34Z ALTAR3Q $
+	-------------------------------------------------
+**/
+jQuery(document).ready(function($){
+
+	// Scroll Bar
+	$('#ScrollBar').scrollFollow();	
+	
+    // custom checkboxes
+	$('input[type=checkbox]').checkbox();
+	
+	// Ribbon
+	$(".ribbon .wrapAround").KleejaRibbon();
+	
+	// Tooltip
+	$('.HTI a img').qtip({
+			position: {my: 'bottom right',at: 'top center', adjust: { x:2,  y : 3 , screen: 'flip'}},
+			show: {delay: 0},style: {classes: 'ui-tooltip-plain', tip: true}
+	});
+	$('.seTooltip a').qtip({
+		position: {	my: 'bottom right',	at: 'top right',target: 'mouse',adjust: { y :-7, screen: 'flip'}},
+		style: {classes: 'ui-tooltip-light',tip: true}
+	});
+	$('#ktipTop a img').qtip({
+		position: {target: 'mouse',adjust: {x: 7, y: 7, screen: 'flip'}},
+		style: {classes: 'ui-tooltip-dark',tip: true}
+	});
+	
+	// System Messages
+	$('.notification').hover(function() {
+		$(this).css('cursor','pointer');
+ 	}, function() {
+		$(this).css('cursor','auto');
+	});
+	$('.notification span').click(function() {
+		$(this).parent().fadeOut(800);
+    });
+	$('.notification').click(function() {
+        $(this).fadeOut(800);
+    });
+	
+});

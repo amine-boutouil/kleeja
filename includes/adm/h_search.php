7cb8f371fe67cc95a829d3e0a977789e9a8bbb2c
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2011-03-26 18:36:06 +0000

    Adding search items to GO menu, اضافة محتويات البحث ضمن قائمة قو الجديدة

--- includes/adm/h_search.php
+++ includes/adm/h_search.php
@@ -26,3 +26,17 @@ $default_user_system = (int) $config['user_system'] == 1 ? true : false;
 
 $H_FORM_KEYS	= kleeja_add_form_key('adm_files_search');
 $H_FORM_KEYS2	= kleeja_add_form_key('adm_users_search');
+
+$current_smt	= isset($_GET['smt']) ? (preg_match('![a-z0-9_]!i', trim($_GET['smt'])) ? trim($_GET['smt']) : 'files') : 'files';
+
+
+//secondary menu
+$go_menu = array(
+				'files' => array('name'=>$lang['R_SEARCH'], 'link'=> basename(ADMIN_PATH) . '?cp=h_search&amp;smt=files', 'goto'=>'files', 'current'=> $current_smt == 'files'),
+				'users' => array('name'=>$lang['SEARCH_USERS'], 'link'=> basename(ADMIN_PATH) . '?cp=h_search&amp;smt=users', 'goto'=>'users', 'current'=> $current_smt == 'users'),
+	);
+	
+if(!$default_user_system)
+{
+	unset($go_menu['users']);
+}

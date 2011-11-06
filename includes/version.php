1192a71e7466c703fe0154effcb33726e22cf736
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2011-11-06 02:06:08 +0000

    fix bugs in urls - 2

--- includes/version.php
+++ includes/version.php
@@ -21,7 +21,7 @@ $dev_m = '';
 if(defined('DEV_STAGE'))
 {
 	$dev_verr = preg_match('!.php ([0-9]+) 2!', '$Id$', $m);
-	$dev_m = '#dev' . $m[0];
+	$dev_m = '#dev' . $m[1];
 }
 
 define ('KLEEJA_VERSION' , '1.0.1' . $dev_m);

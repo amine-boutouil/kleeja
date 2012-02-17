631f4f29b091bab12426ec9107d6364320323786
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2012-02-17 03:07:24 +0000

    implement the first codes of groups system اضافة الاكواد الاولية لنظام المجموعات

---
+++ trunk/install/includes/update_files/1.0_to_1.5.php
@@ -0,0 +1,46 @@
+<?php
+// not for directly open
+if (!defined('IN_COMMON'))
+{
+	exit();
+}
+
+//
+//db version when this update was released
+//
+define ('DB_VERSION' , '8');
+
+$update_sqls['groups'] = "
+CREATE TABLE `{$dbprefix}groups` (
+  `group_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `group_is_default` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `group_is_essential` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`group_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+";
+
+$update_sqls['groups_data'] = "
+CREATE TABLE `{$dbprefix}groups_data` (
+  `group_id` int(11) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `value` varchar(255) COLLATE utf8_bin NOT NULL,
+  UNIQUE KEY `name` (`name`),
+  KEY `group_id` (`group_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+";
+
+$update_sqls['groups_acl'] = "
+CREATE TABLE `{$dbprefix}groups_acl` (
+  `acl_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `group_id` int(11) unsigned NOT NULL,
+  `acl_can` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  KEY `group_id` (`group_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+";
+
+$update_sqls['groups_insert'] = "INSERT INTO `{$dbprefix}groups` (`group_id`, `group_name`, `group_is_default`, `group_is_essential`) VALUES
+(1, '{lang.ADMINS}', 0, 1),
+(2, '{lang.GUESTS}', 0, 1),
+(3, '{lang.USERS}', 1, 1),
+(4, 'برونزي', 0, 0);";

3af503aab02f3e14c9c30a0722e1aac58c169eb1
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2012-08-29 10:05:22 +0000

    fix search by IP In files

--- includes/adm/c_files.php
+++ includes/adm/c_files.php
@@ -292,7 +292,7 @@ if ($nums_rows > 0)
 						'report'	=> ($row['report'] > 4) ? "<span style=\"color:red;font-weight:bold\">" . $row['report'] . "</span>":$row['report'],
 						'user'		=> ($row['user'] == '-1') ? $lang['GUST'] :  '<a href="' . $userfile . '" target="_blank">' . $row['username'] . '</a>',
 						'ip'		=> '<a href="http://www.ripe.net/whois?form_type=simple&amp;full_query_string=&amp;searchtext=' . $row['user_ip'] . '&amp;do_search=Search" target="_new">' . $row['user_ip'] . '</a>',
-						'showfilesbyip' => basename(ADMIN_PATH) . '?cp=' . basename(__file__, '.php') . '&search=' . kleeja_base64_encode(serialize(array('user_ip' => $row['user_ip']))),
+						'showfilesbyip' => basename(ADMIN_PATH) . '?cp=h_search&s_input=1&amp;s_value=' . $row['user_ip']
 					);
 
 		$del[$row['id']] = isset($_POST['del_' . $row['id']]) ? $_POST['del_' . $row['id']] : '';

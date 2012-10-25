9da9d107dd29e352d25a7d0e2da8a7c7a1f8b5dc
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2012-10-25 03:21:26 +0000

    fix bugs wih sqls

--- trunk/install/includes/update_files/1.0_to_1.5.php
+++ trunk/install/includes/update_files/1.0_to_1.5.php
@@ -14,45 +14,6 @@ define ('DB_VERSION' , '8');
 #configs, groups_data .. things are not quite simple here
 include dirname(__file__) . '/../default_values.php';
 
-$update_sqls['configs_2_groups'] = "UPDATE  `{$dbprefix}config` SET  `type` =  'groups' WHERE `name` IN('language', 'enable_userfile', 'filesnum', 'sec_down', 'write_imgs', 'usersectoupload');";
-
-foreach($config_values as $m=>$t)
-{
-	if($t[4] == 'groups')
-	{
-		$update_sqls['groups_data_' . $t[0]] = "INSERT INTO `{$dbprefix}groups_data` (`name`, `value`, `group_id`) VALUES ('" . $m[0] . "', '" . $m[1] . "', 1), ('" . $m[0] . "', '" . $m[1] . "', 2), ('" . $m[0] . "', '" . $m[1] . "', 3);";
-	}
-}
-
-
-#new configs
-$update_sqls['config_default_group'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('default_group', '3', '', '', '0', '0', '0');";
-$update_sqls['time_zone'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('time_zone', '3', '<select name=\"time_zone\" id=\"time_zone\">\r\n {time_zones}\r\n </select>', '10', 'general', '0', '0');";
-
-#remove plugin kleeja-live-folder-name
-if(($klive_id = kleeja_plugin_exists('kleeja-live-folder-name')))
-{
-	$update_sqls['klive_delete1'] = "DELETE FROM `{$dbprefix}hooks` WHERE plg_id = $klive_id ;";
-	$update_sqls['klive_delete2'] = "DELETE FROM `{$dbprefix}plugins` WHERE plg_id = $klive_id ;";
-}
-else
-{
-	$update_sqls['klive1'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('imagefolder', 'uploads', '<input type=\"text\" id=\"imagefolder\" name=\"imagefolder\" value=\"{con.imagefolder}\" size=\"40\">', '10', 'KLIVE', '0', '0');";
-	$update_sqls['klive2'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('imagefolderexts', '', '<input type=\"text\" id=\"imagefolderexts\" name=\"imagefolderexts\" value=\"{con.imagefolderexts}\" size=\"80\">', '20', 'KLIVE', '0', '0');";
-	$update_sqls['klive3'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('imagefoldere', '1', '<label>{lang.YES}<input type=\"radio\" id=\"imagefoldere\" name=\"imagefoldere\" value=\"1\"  <IF NAME=\"con.imagefoldere\"> checked=\"checked\"</IF>></label><label>{lang.NO}<input type=\"radio\" id=\"imagefoldere\" name=\"imagefoldere\" value=\"0\"  <IF NAME=\"con.imagefoldere\"> <ELSE> checked=\"checked\"</IF>></label>', '30', 'KLIVE', '0', '0');";
-}
-
-
-#register time to users table
-$update_sqls['register_time'] = "ALTER TABLE  `{$dbprefix}users` ADD  `register_time` INT( 11 ) UNSIGNED NOT NULL DEFAULT  '0' AFTER  `last_visit`;";
-
-#alter stats table
-$update_sqls['alter_stats_add_imgs'] = "ALTER TABLE  `{$dbprefix}stats` ADD  `imgs` INT( 11 ) UNSIGNED NOT NULL DEFAULT  '0' AFTER  `files`;";
-
-#reset stats
-$update_sqls['stats_to_0'] = "UPDATE  `{$dbprefix}stats` SET  `files` =  '0', `imgs` =  '0';";
-
-
 #new tables
 $update_sqls['filters'] = "
 CREATE TABLE `{$dbprefix}filters` (
@@ -96,6 +57,46 @@ CREATE TABLE `{$dbprefix}groups_acl` (
   KEY `group_id` (`group_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 ";
+
+
+$update_sqls['configs_2_groups'] = "UPDATE  `{$dbprefix}config` SET  `type` =  'groups' WHERE `name` IN('language', 'enable_userfile', 'filesnum', 'sec_down', 'write_imgs', 'usersectoupload');";
+
+foreach($config_values as $m=>$t)
+{
+	if($t[4] == 'groups')
+	{
+		$update_sqls['groups_data_' . $t[0]] = "INSERT INTO `{$dbprefix}groups_data` (`name`, `value`, `group_id`) VALUES ('" . $m[0] . "', '" . $m[1] . "', 1), ('" . $m[0] . "', '" . $m[1] . "', 2), ('" . $m[0] . "', '" . $m[1] . "', 3);";
+	}
+}
+
+
+#new configs
+$update_sqls['config_default_group'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('default_group', '3', '', '', '0', '0', '0');";
+$update_sqls['time_zone'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('time_zone', '3', '<select name=\"time_zone\" id=\"time_zone\">\r\n {time_zones}\r\n </select>', '10', 'general', '0', '0');";
+
+#remove plugin kleeja-live-folder-name
+if(($klive_id = kleeja_plugin_exists('kleeja-live-folder-name')))
+{
+	$update_sqls['klive_delete1'] = "DELETE FROM `{$dbprefix}hooks` WHERE plg_id = $klive_id ;";
+	$update_sqls['klive_delete2'] = "DELETE FROM `{$dbprefix}plugins` WHERE plg_id = $klive_id ;";
+}
+else
+{
+	$update_sqls['klive1'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('imagefolder', 'uploads', '<input type=\"text\" id=\"imagefolder\" name=\"imagefolder\" value=\"{con.imagefolder}\" size=\"40\">', '10', 'KLIVE', '0', '0');";
+	$update_sqls['klive2'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('imagefolderexts', '', '<input type=\"text\" id=\"imagefolderexts\" name=\"imagefolderexts\" value=\"{con.imagefolderexts}\" size=\"80\">', '20', 'KLIVE', '0', '0');";
+	$update_sqls['klive3'] = "INSERT INTO `{$dbprefix}config` (`name`, `value`, `option`, `display_order`, `type`, `plg_id`, `dynamic`) VALUES ('imagefoldere', '1', '<label>{lang.YES}<input type=\"radio\" id=\"imagefoldere\" name=\"imagefoldere\" value=\"1\"  <IF NAME=\"con.imagefoldere\"> checked=\"checked\"</IF>></label><label>{lang.NO}<input type=\"radio\" id=\"imagefoldere\" name=\"imagefoldere\" value=\"0\"  <IF NAME=\"con.imagefoldere\"> <ELSE> checked=\"checked\"</IF>></label>', '30', 'KLIVE', '0', '0');";
+}
+
+
+#register time to users table
+$update_sqls['register_time'] = "ALTER TABLE  `{$dbprefix}users` ADD  `register_time` INT( 11 ) UNSIGNED NOT NULL DEFAULT  '0' AFTER  `last_visit`;";
+
+#alter stats table
+$update_sqls['alter_stats_add_imgs'] = "ALTER TABLE  `{$dbprefix}stats` ADD  `imgs` INT( 11 ) UNSIGNED NOT NULL DEFAULT  '0' AFTER  `files`;";
+
+#reset stats
+$update_sqls['stats_to_0'] = "UPDATE  `{$dbprefix}stats` SET  `files` =  '0', `imgs` =  '0';";
+
 
 $update_sqls['groups_insert'] = "INSERT INTO `{$dbprefix}groups` (`group_id`, `group_name`, `group_is_default`, `group_is_essential`) VALUES
 (1, '{lang.ADMINS}', 0, 1),

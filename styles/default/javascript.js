88e3664c0e396d5e26fcab53327dd88679773e3c
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2013-02-14 23:21:13 +0000

    fix bug at do.php?thumbf + good look of userfile

--- styles/default/javascript.js
+++ styles/default/javascript.js
@@ -92,7 +92,7 @@
 	}
 
 	/** IE > Suckerfish :focus **/	
-	sfFocus = function() {var sfEls = document.getElementsByTagName("INPUT");for (var i=0; i<sfEls.length; i++) {sfEls[i].onfocus=function() {this.className+=" sffocus";};sfEls[i].onblur=function() {this.className=this.className.replace(new RegExp(" sffocus\\b"), "");};};};if (window.attachEvent) window.attachEvent("onload", sfFocus);
+	//sfFocus = function() {var sfEls = document.getElementsByTagName("INPUT");for (var i=0; i<sfEls.length; i++) {sfEls[i].onfocus=function() {this.className+=" sffocus";};sfEls[i].onblur=function() {this.className=this.className.replace(new RegExp(" sffocus\\b"), "");};};};if (window.attachEvent) window.attachEvent("onload", sfFocus);
 	
 	// Add a getElementsByClassName function if the browser doesn't have one
 	// Limitation: only works with one class name

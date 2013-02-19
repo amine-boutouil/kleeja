cf51f389ce59d2651c9b3a00cfc41a5f2a6cf143
developer.mahmoud <developer.mahmoud@940b39b9-983a-0410-8f00-47514fe486d8>
2013-02-19 12:25:57 +0000

    fix problem in tabs and update old javascript

--- styles/default/javascript.js
+++ styles/default/javascript.js
@@ -8,76 +8,26 @@
 	-------------------------------------------------
 **/
 
-	//to enable mutli onload
-	function addLoadEvent(func)
-	{
-		var oldonload = window.onload;
-		if (typeof window.onload != 'function'){
-			window.onload = func;
-		}else{
-			window.onload = function(){
-				if (oldonload){
-					oldonload();
-				}
-				func();
-			}
-		}
-	}
-
-	//new field
-	function plus (id, type)
-	{
-		id = id+1;
-		var input_id = (type==1) ?  'file' : 'fileu';
-		var br_id = (type==1) ?  'br' : 'bru';
+	$(document).ready(function(){
 		
-		if (id < number_of_uploads+1)
-		{
-			eval('var s = "' + input_id +'_' + id + '_";var br = "' + br_id + '_' + id + '_";');
-			$(s).style.display = '';
-			$(br).style.display = '';			
-		}
-		else
-		{
-			alert(LANG_MORE_F_FILES);
-		}
-	}
+		//display new input until it exceed maximum input
+		$('.file,.url').change(function(){
+			var i = this.name.replace(/file|_/g,'');
+			
+			if( i >= number_of_uploads ){alert(LANG_MORE_F_FILES);return;};
+			
+			if(this.type == 'file') $('.file:eq('+ i +')').css('display', 'block');
+			else $('.url:eq('+ i +')').css('display', 'block');
+		});
+		
+		//display loader while upload files
+		$('#uploader').submit(function(){
+			$('#loadbox').css('display', 'block');
+			$('#uploader').css('display', 'none');
+		});
 
-	//submit
-	function form_submit() 
-	{
-		setTimeout
-		( 
-			function() 
-			{ 
-				var load = $('loadbox');
-				load.style.display='inline'; 
-				load.src='ajax-loader.gif'
-			},
-			500
-		)
-		var uploader = $("uploader");
-		uploader.style.display = "none";
-		document.uploader.submit();
-	}
 
-	//show or hide some fields
-	function showhide()
-	{
-		var txt = $("texttype");
-		var fle = $("filetype");
-
-		if (txt.style.display == 'none')
-		{
-			txt.style.display = 'block';
-			fle.style.display = 'none';
-		}
-		else
-		{
-			fle.style.display = 'block';
-			txt.style.display = 'none';
-		}
-	}
+	});
 	
 	//
 	//javascript for captcha
@@ -91,12 +41,5 @@
 		img.src = captcha_file + '&' + Math.random();
 	}
 
-	/** IE > Suckerfish :focus **/	
-	//sfFocus = function() {var sfEls = document.getElementsByTagName("INPUT");for (var i=0; i<sfEls.length; i++) {sfEls[i].onfocus=function() {this.className+=" sffocus";};sfEls[i].onblur=function() {this.className=this.className.replace(new RegExp(" sffocus\\b"), "");};};};if (window.attachEvent) window.attachEvent("onload", sfFocus);
-	
-	// Add a getElementsByClassName function if the browser doesn't have one
-	// Limitation: only works with one class name
-	// Copyright: Eike Send http://eike.se/nd
-	// License: MIT License
-	// modified by Kleeja
+	//javascript for tabs
 	function tabs(c1,c2,c3,c4){var m2,m3,l,t;var onclickf=function(){if(l<1)return 0;for(t=0;t<l;t++)if(m2[t]!=this){m2[t].className="";m3[t].style.display="none"}else{m2[t].className=c4;m3[t].style.display="block"}};var cl=function(){if(l<1)return 0;m2[0].className=c4;m3[0].style.display="block";for(t=0;t<l;t++)m2[t].onclick=onclickf};m2=$(c1+" "+c2+" li");if(m2[0]!="undefined"){m3=$(c1+" "+c3);l=m2.length;cl()}}$(document).ready(function(){var mah;mah=new tabs(".tabmain",".tabnav",".tabcon","tabactive")});

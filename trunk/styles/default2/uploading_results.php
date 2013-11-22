7fd4588b7a1566f3e9c6d5c4e287117c2b611326
saanina <saanina@940b39b9-983a-0410-8f00-47514fe486d8>
2013-11-22 20:09:56 +0000

    changes toward 2.0

--- trunk/styles/default2/uploading_results.php
+++ trunk/styles/default2/uploading_results.php
@@ -24,38 +24,38 @@
 <!-- Results -->
 <?php foreach($RESULTS as $result):?>
 
-	<?php if(isset($result['thumb'])):?>
-	
-		<div class="thumb_img"><a onclick="window.open(this.href,'_blank');return false;" href="{b_url_link}"><img src="{b_img_link}" alt="" /></a></div>
-	
-		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
-			<tr>
-				<td class="btitle">{b_title}</td>
-				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="1" onclick="this.select();">[url={b_url_link}][img]<?=$result['thumb']?>[/img][/url]</textarea></td>
-			</tr>
-		</table>
-	<?php endif?>
-
 	<?php if(isset($result['image'])):?>
 
+		<?php if(isset($result['thumb'])):?>
+	
+			<div class="thumb_img"><a onclick="window.open(this.href,'_blank');return false;" href="<?=$result['image']?>"><img src="<?=$result['thumb']?>" alt="" /></a></div>
+	
+			<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
+				<tr>
+					<td class="btitle"><?=$lang['URL_F_THMB']?></td>
+					<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="1" onclick="this.select();">[url=<?=$result['image']?>][img]<?=$result['thumb']?>[/img][/url]</textarea></td>
+				</tr>
+			</table>
+		<?php endif?>
+
 		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
 			<tr>
-				<td class="btitle">{b_title}</td>
-				<td class="all_box_link"><textarea rows="1" cols="40" class="up_input" tabindex="2" onclick="this.select();">{b_url_link}</textarea></td>
+				<td class="btitle"><?=$lang['URL_F_IMG']?></td>
+				<td class="all_box_link"><textarea rows="1" cols="40" class="up_input" tabindex="2" onclick="this.select();"><?=$result['image']?></textarea></td>
 			</tr>
 		</table>
 
 		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
 			<tr>
-				<td class="btitle">{b_bbc_title}</td>
-				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="3" onclick="this.select();">[url={siteurl}][img]{b_url_link}[/img][/url]</textarea></td>
+				<td class="btitle"><?=$lang['URL_F_BBC']?></td>
+				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="3" onclick="this.select();">[url=<?=$config['siteurl']?>][img]<?=$result['image']?>[/img][/url]</textarea></td>
 			</tr>
 		</table>
 
 		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
 			<tr>
 				<td class="btitle">HTML</td>
-				<td class="all_box_link"><textarea rows="4" cols="40" class="up_input" tabindex="4" onclick="this.select();">&lt;a href=&quot;{siteurl}&quot; target=&quot;_blank&quot; title=&quot;{siteurl}&quot;&gt;&lt;img src=&quot;{b_url_link}&quot; border=&quot;0&quot; alt=&quot;{b_url_link}&quot; /&gt;&lt;/a&gt;</textarea></td>
+				<td class="all_box_link"><textarea rows="4" cols="40" class="up_input" tabindex="4" onclick="this.select();">&lt;a href=&quot;<?=$config['siteurl']?>&quot; target=&quot;_blank&quot;&gt;&lt;img src=&quot;<?=$result['image']?>&quot; border=&quot;0&quot; /&gt;&lt;/a&gt;</textarea></td>
 			</tr>
 		</table>
 	<?php endif?>
@@ -64,15 +64,15 @@
 
 		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
 			<tr>
-				<td class="btitle">{b_title}</td>
-				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="5" onclick="this.select();">{b_url_link}</textarea></td>
+				<td class="btitle"><?=$lang['URL_F_FILE']?></td>
+				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="5" onclick="this.select();"><?=$result['file']?></textarea></td>
 			</tr>
 		</table>
 	
 		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
 			<tr>
-				<td class="btitle">{b_bbc_title}</td>
-				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="6" onclick="this.select();">[url={b_url_link}]{b_url_link}[/url]</textarea></td>
+				<td class="btitle"><?=$lang['URL_F_BBC']?></td>
+				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="6" onclick="this.select();">[url=<?=$result['file']?>]<?=$result['file']?>[/url]</textarea></td>
 			</tr>
 		</table>
 	<?php endif?>
@@ -81,8 +81,8 @@
 
 		<table border="0" width="100%" cellspacing="0" cellpadding="0" class="up_box_input">
 			<tr>
-				<td class="btitle">{b_title}</td>
-				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="7" onclick="this.select();">{b_code_link}</textarea></td>
+				<td class="btitle"><?=$lang['URL_F_DEL']?></td>
+				<td class="all_box_link"><textarea rows="2" cols="40" class="up_input" tabindex="7" onclick="this.select();"><?=$result['delete_code']?></textarea></td>
 			</tr>
 		</table>
 
